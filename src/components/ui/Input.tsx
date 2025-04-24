c623c61f1186a06f744650c296e709272a585b68
nninyeong <minyk0512@gmail.com>
2025-04-25 02:35:17 +0900

    feat: 글자수/최대글자 보여주는 기능 추가

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -6,6 +6,7 @@ type InputProps<T extends FieldValues> = {
   type?: 'text' | 'password' | 'email';
   placeholder?: string;
   maxLength?: number;
+  showCharacterCount?: boolean;
   showPasswordToggle?: boolean;
 };
 
@@ -15,6 +16,7 @@ export default function Input<T extends FieldValues>({
   type = 'text',
   maxLength,
   placeholder,
+  showCharacterCount = false,
   showPasswordToggle = false,
 }: InputProps<T>) {
   return (
@@ -22,13 +24,23 @@ export default function Input<T extends FieldValues>({
       name={name}
       control={control}
       render={({ field }) => (
-        <input
-          {...field}
-          type={type}
-          maxLength={maxLength}
-          placeholder={placeholder}
-          className='w-full h-[50px] px-[20px] py-[15px] rounded-[10px] bg-white disabled:bg-gray-10 border-[1px] border-gray-20 focus:border-[1px] focus:border-purple-50 focus:outline-none text-body-reading text-gray-90 placeholder:text-gray-50'
-        />
+        <div className='relative w-full'>
+          <input
+            {...field}
+            type={type}
+            maxLength={maxLength}
+            placeholder={placeholder}
+            className='absolute w-full h-[50px] px-[20px] py-[15px] rounded-[10px] bg-white disabled:bg-gray-10 border-[1px] border-gray-20 focus:border-[1px] focus:border-purple-50 focus:outline-none text-body-reading text-gray-90 placeholder:text-gray-50'
+          />
+          <div className='absolute right-[20px] top-1/2 translate-y-1/2 z-[1]'>
+            {showCharacterCount && (
+              <div className='flex flex-row gap-[1px] justify-between items-center text-headline1 text-gray-50'>
+                <span className='text-gray-70 font-semibold'>{field.value?.length ?? 0}</span>
+                <span className='text-gray-50 font-regular'>/{maxLength}</span>
+              </div>
+            )}
+          </div>
+        </div>
       )}
     />
   );
