82096f5c65fecd87e1862fa4df976dafb83b3293
nninyeong <minyk0512@gmail.com>
2025-04-28 01:48:53 +0900

    chore: minimal 레이아웃 폴더 경로 수정

--- src/app/(auth)/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <main className='flex-1 flex flex-col justify-center items-center bg-white'>{children}</main>;
-}
---
+++ src/app/(minimal)/layout.tsx
@@ -0,0 +1,3 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <main className='flex-1 flex flex-col justify-center items-center w-full h-full'>{children}</main>;
+}
