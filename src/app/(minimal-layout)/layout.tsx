8058740583aa273248bc9722aae28c1afa1b4215
nninyeong <minyk0512@gmail.com>
2025-04-28 16:32:59 +0900

    style: 레이아웃 최소 높이 설정

--- src/app/(minimal-layout)/layout.tsx
+++ src/app/(minimal-layout)/layout.tsx
@@ -1,3 +1,5 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <main className='flex-1 flex flex-col justify-center items-center w-full h-full'>{children}</main>;
+  return (
+    <main className='flex-1 flex flex-col justify-center items-center w-full h-full min-h-screen'>{children}</main>
+  );
 }
