159b543d4ae4b988bca66460ed2455d17a7acf21
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-04-30 17:54:50 +0900

    style: bg 색상 추가

--- src/app/(navigation-layout)/layout.tsx
+++ src/app/(navigation-layout)/layout.tsx
@@ -2,7 +2,7 @@ import { SideNavWrapper } from './SideNavWrapper';
 
 export default function NavigationLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex justify-between w-[1320px] mx-auto'>
+    <div className='flex justify-between w-[1320px] mx-auto bg-gray-10'>
       <SideNavWrapper />
       <main className='w-[1018px]'>{children}</main>
     </div>
