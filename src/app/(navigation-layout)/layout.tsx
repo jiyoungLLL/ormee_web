db7afb652d3eefb90c489d2f46ca5fee4ad2fbdb
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-05-03 17:00:27 +0900

    style: height 100vh로 고정

--- src/app/(navigation-layout)/layout.tsx
+++ src/app/(navigation-layout)/layout.tsx
@@ -3,10 +3,10 @@ import { SideNavWrapper } from './SideNavWrapper';
 
 export default function NavigationLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex flex-col bg-gray-10 items-center'>
-      <div className='flex flex-col justify-start items-start w-full max-w-[1320px] h-full mx-auto'>
+    <div className='flex flex-col h-[100vh] bg-gray-10 items-center'>
+      <div className='flex flex-col justify-start items-start w-full max-w-[1320px] mx-auto'>
         <Header />
-        <div className='flex justify-between w-[1320px] h-[100vh] mx-auto bg-gray-10'>
+        <div className='flex justify-between w-[1320px] mx-auto bg-gray-10'>
           <SideNavWrapper />
           <main className='w-[1018px]'>{children}</main>
         </div>
