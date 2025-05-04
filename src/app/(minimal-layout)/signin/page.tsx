0c775793a049f2aad288c50b6dc10c587cf341bc
nninyeong <minyk0512@gmail.com>
2025-05-04 19:40:35 +0900

    feat: 로그인 폼 react-hook-form 연동, 공용 컴포넌트로 변경

--- src/app/(minimal-layout)/signin/page.tsx
+++ src/app/(minimal-layout)/signin/page.tsx
@@ -1,7 +1,24 @@
+'use client';
+
+import Button from '@/components/ui/Button';
+import Input from '@/components/ui/Input';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 
 export default function SignInPage() {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      id: '',
+      password: '',
+    },
+  });
+
+  const handleSignIn = (data: any) => {
+    // TODO: 로그인 요청 API 연동
+    alert(`로그인 요청: ${data.id}, ${data.password}`);
+  };
+
   return (
     <div className='flex flex-col justify-center items-center gap-[50px] w-[400px] h-fit'>
       <div className='flex flex-col justify-center items-center gap-[20px]'>
@@ -13,25 +30,41 @@ export default function SignInPage() {
         />
         <h1 className='text-gray-70 text-heading2 font-semibold'>체계적인 수업 관리 서비스 오르미</h1>
       </div>
-      <div className='flex flex-col justify-center items-center gap-[12px]'>
-        {/* TODO: 공통 컴포넌트로 수정 Input, Button */}
-        <input
-          type='text'
-          placeholder='아이디'
-        />
-        <input
-          type='password'
-          placeholder='비밀번호'
-        />
-      </div>
-      <div className='flex flex-col justify-center items-center gap-[30px]'>
-        <button>로그인</button>
-        <div className='flex flex-row justify-center items-center gap-[16px] text-gray-70 text-body1-normal'>
-          <Link href='#'>아이디/비번 찾기</Link>
-          <span className='text-gray-30'>|</span>
-          <Link href='/signup'>회원가입</Link>
+      <form onSubmit={handleSubmit(handleSignIn)}>
+        <div className='flex flex-col justify-center items-center w-[400px] gap-[12px] mb-[50px]'>
+          <Input
+            name='id'
+            control={control}
+            size='w-full h-[50px]'
+            placeholder='아이디'
+            type='text'
+          />
+          <Input
+            name='password'
+            control={control}
+            size='w-full h-[50px]'
+            placeholder='비밀번호'
+            type='password'
+            showPasswordToggle
+          />
         </div>
-      </div>
+        <div className='flex flex-col justify-center items-center gap-[30px]'>
+          <Button
+            type='BUTTON_BASE_TYPE'
+            title='로그인'
+            isPurple
+            isfilled
+            width={400}
+            height={60}
+            htmlType='submit'
+          />
+          <div className='flex flex-row justify-center items-center gap-[16px] text-gray-70 text-body1-normal'>
+            <Link href='#'>아이디/비번 찾기</Link>
+            <span className='text-gray-30'>|</span>
+            <Link href='/signup'>회원가입</Link>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
