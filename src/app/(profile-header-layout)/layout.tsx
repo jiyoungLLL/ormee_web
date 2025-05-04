9367757140f1e3213d15013545d6600e583b1074
nninyeong <minyk0512@gmail.com>
2025-05-05 02:55:27 +0900

    feat: 헤더에 알림 아이콘 버튼 추가

--- src/app/(profile-header-layout)/layout.tsx
+++ src/app/(profile-header-layout)/layout.tsx
@@ -1,12 +1,16 @@
 import Header from '@/components/layouts/Header';
 import HeaderProfile from '@/components/profiles/HeaderProfile';
+import Notification from '@/components/notification/Notification';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex-1 w-full min-h-screen bg-gray-10'>
       <div className='flex flex-col justify-start items-start w-full max-w-[1320px] h-full mx-auto'>
         <Header>
-          <HeaderProfile />
+          <div className='flex flex-row justify-between items-center gap-[30px]'>
+            <Notification />
+            <HeaderProfile />
+          </div>
         </Header>
         <main className='flex-1 flex flex-col justify-center items-center w-full h-full'>{children}</main>
       </div>
