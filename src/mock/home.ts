42398ade813120d6cad3dbcd6bf1c84b6f2b3d47
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-05-07 23:18:08 +0900

    chore: 변수명 변경

--- src/mock/home.ts
+++ src/mock/home.ts
@@ -1,6 +1,6 @@
-import { homeSlide } from '@/types/home.types';
+import { HomeSlideProps } from '@/types/home.types';
 
-const rawData: Record<number, homeSlide> = {
+const rawData: Record<number, HomeSlideProps> = {
   0: { cate: '퀴즈', title: '2주차 단어', student: '24/50', date: '2025.02.23 - 2025.04.02' },
   1: { cate: '숙제', title: '2주차 단어', student: '24/50', date: '2025.02.23 - 2025.04.02' },
   2: { cate: '퀴즈', title: '2주차 단어', student: '24/50', date: '2025.02.23 - 2025.04.02' },
@@ -8,4 +8,4 @@ const rawData: Record<number, homeSlide> = {
   4: { cate: '퀴즈', title: '2주차 단어', student: '24/50', date: '2025.02.23 - 2025.04.02' },
 };
 
-export const MOCK_HOME_SLIDE: homeSlide[] = Object.values(rawData);
+export const MOCK_HOME_SLIDE: HomeSlideProps[] = Object.values(rawData);
--- src/types/home.types.ts
+++ src/types/home.types.ts
@@ -1,6 +1,12 @@
-export type homeSlide = {
+export type HomeSlideProps = {
   cate: string;
   title: string;
   student: string;
   date: string;
+};
+
+export type ScrollButtonProps = {
+  direction: 'left' | 'right';
+  onClick: () => void;
+  visible: boolean;
 };
