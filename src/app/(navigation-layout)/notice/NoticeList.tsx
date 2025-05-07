4c6d3b147f287a21d44f80477c94f629c59b6aec
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-05-07 23:17:15 +0900

    refactor: 공지 목데이터

--- src/app/(navigation-layout)/notice/NoticeList.tsx
+++ src/app/(navigation-layout)/notice/NoticeList.tsx
@@ -1,24 +1,7 @@
+import { tempData } from '@/mock/notice';
 import Image from 'next/image';
 
 export default function NoticeList() {
-  // 임시 데이터
-  const tempData: { [key: string]: { [innerKey: string]: string | number } } = {
-    pin: {
-      number: 1,
-      title: '출석 체크 꼭 하세요',
-      writer: '강수이',
-      date: '2025.01.20',
-      views: 11,
-    },
-    list: {
-      number: 1,
-      title: '지우개 훔친 사람 자수해요',
-      writer: '강수이',
-      date: '2025.01.20',
-      views: 1321,
-    },
-  };
-
   // list title
   const titleList: [string, string][] = [
     ['No.', 'w-[34px]'],
