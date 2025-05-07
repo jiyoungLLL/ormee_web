682f6cee7c12c4683181ca05f6e95a093593f5c9
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-05-07 21:39:36 +0900

    feat: 홈 슬라이드 목 데이터

---
+++ src/mock/homeSlide.ts
@@ -0,0 +1,11 @@
+import { homeSlide } from '@/types/homeSlide.types';
+
+const rawData: Record<number, homeSlide> = {
+  0: { cate: '퀴즈', title: '2주차 단어', student: '24/50', date: '2025.02.23 - 2025.04.02' },
+  1: { cate: '숙제', title: '2주차 단어', student: '24/50', date: '2025.02.23 - 2025.04.02' },
+  2: { cate: '퀴즈', title: '2주차 단어', student: '24/50', date: '2025.02.23 - 2025.04.02' },
+  3: { cate: '숙제', title: '2주차 단어', student: '24/50', date: '2025.02.23 - 2025.04.02' },
+  4: { cate: '퀴즈', title: '2주차 단어', student: '24/50', date: '2025.02.23 - 2025.04.02' },
+};
+
+export const MOCK_HOME_SLIDE: homeSlide[] = Object.values(rawData);
