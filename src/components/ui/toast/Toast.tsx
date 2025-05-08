0dc05cce4155f2b8ecdc9eeb5e81bc5e63d2d91b
nninyeong <minyk0512@gmail.com>
2025-05-09 02:11:35 +0900

    feat: 개별 토스트 메세지 영역은 아래쪽으로 포인터 이벤트가 전파되지 않도록 설정

--- src/components/ui/toast/Toast.tsx
+++ src/components/ui/toast/Toast.tsx
@@ -50,7 +50,7 @@ export default function Toast({ id, message, type, duration = 2500 }: ToastProps
   return (
     <div
       id={id}
-      className={`flex flex-row justify-center items-center gap-[10px] w-fit px-[20px] py-[10px] rounded-[10px] text-gray-90 text-headline1 font-semibold ${TOAST_STYLE[type]} transform transition-all duration-${TOAST_ANIMATION_DURATION} ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-[20px]'}`}
+      className={`flex flex-row justify-center items-center gap-[10px] w-fit px-[20px] py-[10px] rounded-[10px] text-gray-90 text-headline1 font-semibold ${TOAST_STYLE[type]} transform transition-all duration-${TOAST_ANIMATION_DURATION} ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-[20px]'} pointer-events-auto`}
     >
       <img
         src={TOAST_ICON[type]}
