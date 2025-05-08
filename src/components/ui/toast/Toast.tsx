9b817a28b347ceac6fd3c75e1c389dca50411e4c
nninyeong <minyk0512@gmail.com>
2025-05-08 17:56:15 +0900

    feat: 일정시간 후 토스트 알림이 사라지는 기능 구현

--- src/components/ui/toast/Toast.tsx
+++ src/components/ui/toast/Toast.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 type ToastProps = {
   id: string;
   message: string;
@@ -15,11 +19,25 @@ const TOAST_STYLE: Record<ToastProps['type'], string> = {
   error: 'bg-state-error',
 };
 
-export default function Toast({ id, message, type, duration = 3000 }: ToastProps) {
+export default function Toast({ id, message, type, duration = 2500 }: ToastProps) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+
+    const toastTimer = setTimeout(() => {
+      setIsVisible(false);
+    }, duration);
+
+    return () => {
+      clearTimeout(toastTimer);
+    };
+  }, []);
+
   return (
     <div
       id={id}
-      className={`flex flex-row justify-center items-center gap-[10px] w-fit px-[20px] py-[10px] rounded-[10px] text-gray-90 text-headline1 font-semibold ${TOAST_STYLE[type]}`}
+      className={`flex flex-row justify-center items-center gap-[10px] w-fit px-[20px] py-[10px] rounded-[10px] text-gray-90 text-headline1 font-semibold ${TOAST_STYLE[type]} transform transition-all duration-${duration} ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-[20px]'}`}
     >
       <img
         src={TOAST_ICON[type]}
