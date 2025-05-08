38f21c314df61efdb9da0573c4d177ea34be2069
nninyeong <minyk0512@gmail.com>
2025-05-08 23:46:48 +0900

    feat: addToast를 사용할 때 랜덤 아이디를 자동으로 생성하도록 구현

--- src/stores/toastStore.ts
+++ src/stores/toastStore.ts
@@ -3,18 +3,23 @@ import { create } from 'zustand';
 
 type ToastStore = {
   toasts: ToastData[];
-  addToast: (toastData: ToastData) => void;
+  addToast: (toastData: Omit<ToastData, 'id'>) => void;
   removeToast: (id: string) => void;
 };
 
 export const useToastStore = create<ToastStore>((set) => {
+  const generateToastId = () => `toast-${crypto.randomUUID()}`;
+
   return {
     toasts: [],
 
-    addToast: (toastData: ToastData) =>
+    addToast: (toastData: Omit<ToastData, 'id'>) => {
+      const id = generateToastId();
+
       set((state: ToastStore) => ({
-        toasts: [...state.toasts, toastData],
-      })),
+        toasts: [...state.toasts, { ...toastData, id }],
+      }));
+    },
 
     removeToast: (id: string) =>
       set((state: ToastStore) => ({
