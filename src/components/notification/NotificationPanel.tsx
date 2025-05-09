3d9c7ea31aa992aaa9da7d770f9306bafe0c3414
nninyeong <minyk0512@gmail.com>
2025-05-10 01:20:04 +0900

    style: 패널 영역 분배 후 각 영역 컨테이너 스타일 지정

--- src/components/notification/NotificationPanel.tsx
+++ src/components/notification/NotificationPanel.tsx
@@ -1,3 +1,6 @@
+import { MOCK_NOTIFICATION_LIST } from '@/mock/notification';
+import { NotificationType } from '@/types/notification.types';
+import { useState } from 'react';
 import { createPortal } from 'react-dom';
 
 type NotificationPanelProps = {
@@ -21,7 +24,19 @@ export default function NotificationPanel({ isOpen, onClose }: NotificationPanel
       className='fixed top-0 left-0 w-full h-dvh bg-gray-90/50'
       onClick={handleClose}
     >
-      <div className='absolute top-0 right-0 flex flex-col gap-[20px] w-[600px] h-full px-[40px] py-[30px] rounded-l-[20px] shadow-[0px_0px_7px_0px_rgba(70,72,84,0.10)] bg-white'></div>
+      <div className='absolute top-0 right-0 flex flex-col items-start gap-[20px] w-[600px] h-full px-[40px] py-[30px] rounded-l-[20px] shadow-[0px_0px_7px_0px_rgba(70,72,84,0.10)] bg-white'>
+        <h2 className='flex items-start gap-[10px] text-heading2 font-semibold'>
+          <span className='text-black'>알림</span>
+          <span className='text-purple-50'>{MOCK_NOTIFICATION_LIST.length}</span>
+        </h2>
+        <div className='flex-1 flex flex-col items-center gap-[10px] self-stretch'>
+          <div className='flex justify-between items-center self-stretch border-b border-gray-30'>
+            {/* 알림 타입 버튼 영역 */}
+          </div>
+          <div className='flex justify-end gap-[10px]'>{/* 모두 읽음, 모두 지움 */}</div>
+          <div className='flex flex-col px-[4px] items-start gap-[12px] self-stretch'>{/* 알림 리스트 영역 */}</div>
+        </div>
+      </div>
     </div>,
     notificationRoot,
   );
