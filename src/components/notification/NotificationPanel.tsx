4c26b718a69273f79595b99bbcd838d723312acc
nninyeong <minyk0512@gmail.com>
2025-05-10 00:47:14 +0900

    fix: 배경 영역을 클릭했을 때만 패널이 닫히도록 수정

--- src/components/notification/NotificationPanel.tsx
+++ src/components/notification/NotificationPanel.tsx
@@ -11,11 +11,18 @@ export default function NotificationPanel({ isOpen, onClose }: NotificationPanel
   const notificationRoot = document.getElementById('notification-root');
   if (!notificationRoot) return;
 
+  const handleClose = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target !== e.currentTarget) return;
+    onClose();
+  };
+
   return createPortal(
     <div
       className='fixed top-0 left-0 w-full h-dvh bg-gray-90/50'
-      onClick={onClose}
-    ></div>,
+      onClick={handleClose}
+    >
+      <div className='absolute top-0 right-0 flex flex-col gap-[20px] w-[600px] h-full px-[40px] py-[30px] rounded-l-[20px] shadow-[0px_0px_7px_0px_rgba(70,72,84,0.10)] bg-white'></div>
+    </div>,
     notificationRoot,
   );
 }
