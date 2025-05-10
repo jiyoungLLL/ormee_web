bc6fe630c7b7b8cd97b400d32af240dab03250cd
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-05-10 20:36:56 +0900

    refactor: 텍스트에디터 placeholer 설정 수정

--- src/components/ui/TiptapEditor.tsx
+++ src/components/ui/TiptapEditor.tsx
@@ -2,30 +2,41 @@
 
 import Heading from '@tiptap/extension-heading';
 import Image from '@tiptap/extension-image';
+import Placeholder from '@tiptap/extension-placeholder';
+import Underline from '@tiptap/extension-underline';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import Toolbar from './Toolbar';
 
 type TipTapProps = {
+  type: '공지' | '과제';
   contents: string;
   onChange: (text: string) => void;
 };
 
-export default function TiptapEditor({ contents, onChange }: TipTapProps) {
+export default function TiptapEditor({ type, contents, onChange }: TipTapProps) {
   const editor = useEditor({
     extensions: [
-      StarterKit,
+      StarterKit.configure({}),
+      Underline,
       Image,
       Heading.configure({
         HTMLAttributes: {
-          class: 'text-xl font-bold',
+          class: 'text-body',
           levels: [2],
         },
+      }),
+      Placeholder.configure({
+        placeholder: `${type}를 입력하세요`,
+        emptyEditorClass:
+          'cursor-text before:content-[attr(data-placeholder)] before:absolute before:top-[20px] before:left-[20px] before:text-mauve-11 before:opacity-50 before-pointer-events-none',
       }),
     ],
     content: contents,
     editorProps: {
       attributes: {
-        class: 'prose prose-sm focus:outline-none min-h-[200px] p-4 border border-gray-300 rounded-md bg-white',
+        class:
+          'focus:border-purple-50 focus:outline-none min-h-[422px] p-[20px] border border-gray-300 rounded-md bg-white overflow-y-auto placeholder:text-gray-40',
       },
     },
     onUpdate: ({ editor }) => {
@@ -36,17 +47,13 @@ export default function TiptapEditor({ contents, onChange }: TipTapProps) {
   const addImage = () => {};
 
   return (
-    <div className='flex flex-col gap-2'>
-      <div className='flex justify-end'>
-        <button
-          type='button'
-          onClick={addImage}
-          className='text-sm text-purple-600 hover:underline'
-        >
-          이미지 추가
-        </button>
-      </div>
-      <EditorContent editor={editor} />
+    <div className='flex flex-col gap-[12px]'>
+      <Toolbar editor={editor} />
+      <EditorContent
+        editor={editor}
+        className='text-body-reading'
+        style={{ height: '422px', borderRadius: '10px' }}
+      />
     </div>
   );
 }
--- src/components/ui/WriteBox.tsx
+++ src/components/ui/WriteBox.tsx
@@ -2,7 +2,11 @@ import { useFormContext } from 'react-hook-form';
 import Button from './Button';
 import TiptapEditor from './TiptapEditor';
 
-export default function NotirceWriteBox() {
+type WriteBoxProps = {
+  type: '공지' | '과제';
+};
+
+export default function WriteBox({ type }: WriteBoxProps) {
   const { control, watch, setValue } = useFormContext();
   const contents = watch('contents');
 
@@ -10,12 +14,14 @@ export default function NotirceWriteBox() {
     <div className='h-auto bg-white p-[30px] rounded-[10px] flex flex-col gap-[10px]'>
       <div className='h-[482px] flex flex-col gap-[12px]'>
         <TiptapEditor
+          type={type}
           contents={contents}
           onChange={(html) => setValue('contents', html)}
         />
       </div>
       <div className='h-[191px] flex flex-col gap-[10px]'>
         <div className='flex items-center h-[48px] gap-[10px]'>
+          <span className='text-heading2 font-semibold p-[10px]'>첨부파일</span>
           <Button
             type='BUTTON_BASE_TYPE'
             size='w-[98px] h-[40px]'
@@ -25,6 +31,9 @@ export default function NotirceWriteBox() {
             isfilled={false}
             htmlType='button'
           />
+        </div>
+        <div className='w-full h-[133px] rounded-[10px] p-[20px] border border-gray-40'>
+          <p className='text-body text-gray-50 font-[18px]'>파일을 마우스로 끌어 놓으세요.</p>
         </div>
       </div>
     </div>
