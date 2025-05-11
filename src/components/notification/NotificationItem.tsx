5a75836721dfc5acbb0d46f10454f52cd352e941
nninyeong <minyk0512@gmail.com>
2025-05-11 15:03:55 +0900

    feat: x 아이콘 이미지 -> 버튼으로 변경, 임시 이벤트 등록

--- src/components/notification/NotificationItem.tsx
+++ src/components/notification/NotificationItem.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Notification } from '@/types/notification.types';
 import NotificationBadge from './NotificationBadge';
 
@@ -7,6 +9,11 @@ type NotificationItemProps = {
 
 export default function NotificationItem({ notification }: NotificationItemProps) {
   const { type, title, description, createdAt, read } = notification;
+
+  const handleDelete = () => {
+    alert('알림 삭제');
+  };
+
   return (
     <div className='flex justify-between items-start w-full py-[12px] self-stretch'>
       <div className='flex items-start'>
@@ -22,9 +29,12 @@ export default function NotificationItem({ notification }: NotificationItemProps
         </div>
         <p className='text-body2 font-normal text-gray-60'>{createdAt}</p>
       </div>
-      <img
-        src='/assets/icons/x.png'
-        className='w-[18px] h-[18px]'
+      <button
+        className='w-[18px] h-[18px] bg-no-repeat bg-center bg-contain'
+        style={{
+          backgroundImage: 'url(/assets/icons/x.png)',
+        }}
+        onClick={handleDelete}
       />
     </div>
   );
