baf36dc7267f514ded0ed4ab87ba62d38b1a38ca
nninyeong <minyk0512@gmail.com>
2025-05-12 00:00:25 +0900

    fix: msw handler notification 목데이터 수정

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -1,9 +1,9 @@
 import { http, HttpResponse } from 'msw';
-import { MOCK_NOTIFICATION_LIST } from './notification';
+import { MOCK_NOTIFICATION_LIST_BULK } from './notification';
 
 export const handlers = [
   http.get('/api/teacher/notification/', () => {
-    return HttpResponse.json(MOCK_NOTIFICATION_LIST, {
+    return HttpResponse.json(MOCK_NOTIFICATION_LIST_BULK, {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
