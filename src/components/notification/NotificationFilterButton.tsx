7fe0d02324d06e4534c680e3fada6e18e1c04c6d
nninyeong <minyk0512@gmail.com>
2025-05-11 16:38:06 +0900

    feat: 알림이 있는 경우 필터 버튼 우측 상단 활성화되도록 구현

--- src/components/notification/NotificationFilterButton.tsx
+++ src/components/notification/NotificationFilterButton.tsx
@@ -1,4 +1,5 @@
 import { NOTIFICATION_TYPE_TEXT } from '@/constants/notification.constants';
+import { MOCK_NOTIFICATION_LIST } from '@/mock/notification';
 import { NotificationFilterType } from '@/types/notification.types';
 
 type NotificationFilterButtonProps = {
@@ -10,6 +11,13 @@ type NotificationFilterButtonProps = {
 export default function NotificationFilterButton({ type, currentType, onClick }: NotificationFilterButtonProps) {
   const isSelected = type === currentType;
 
+  const hasNewNotification = MOCK_NOTIFICATION_LIST.some((notification) => {
+    if (type === 'total') {
+      return !notification.read;
+    }
+    return notification.type === type && !notification.read;
+  });
+
   return (
     <button
       className={`relative flex-1 flex justify-center items-start w-full px-[5px] py-[14px] text-[16px] ${
@@ -17,7 +25,12 @@ export default function NotificationFilterButton({ type, currentType, onClick }:
       }`}
       onClick={() => onClick(type)}
     >
-      {NOTIFICATION_TYPE_TEXT[type]}
+      <span className='relative'>
+        {NOTIFICATION_TYPE_TEXT[type]}
+        {hasNewNotification && (
+          <div className='absolute top-[-2px] right-[-4px] w-[4px] h-[4px] rounded-full bg-purple-50' />
+        )}
+      </span>
       <div
         className={`absolute bottom-0 left-0 w-full ${isSelected ? 'h-[2px] bg-purple-50' : 'h-[1px] bg-gray-30'}`}
       />
