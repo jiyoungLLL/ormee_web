e404910c10e57261d751e4a4944ec77a804e5709
nninyeong <minyk0512@gmail.com>
2025-05-12 19:17:40 +0900

    chore: lectureId 오타 수정

--- src/hooks/queries/useLectureId.ts
+++ src/hooks/queries/useLectureId.ts
@@ -4,7 +4,7 @@ import { useParams } from 'next/navigation';
 
 export const useLectureId = () => {
   const params = useParams();
-  const lectureId = params?.lecutreId as string;
+  const lectureId = params?.lectureId as string;
 
   if (!lectureId) throw new Error('useLectureId 훅이 잘못된 위치에서 호출되었습니다.');
 
