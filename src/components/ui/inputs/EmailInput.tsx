8142fcf8312be253d093a0cf4eeacf245f5a2822
nninyeong <minyk0512@gmail.com>
2025-05-14 02:55:48 +0900

    chore: JSDocs 주석 추가

--- src/components/ui/inputs/EmailInput.tsx
+++ src/components/ui/inputs/EmailInput.tsx
@@ -2,15 +2,28 @@ import { Control, FieldValues, Path } from 'react-hook-form';
 import Input from '../Input';
 
 type EmailInputProps<T extends FieldValues> = {
+  /** useForm에서 사용할 컨트롤러 */
   control: Control<T>;
+  /** 아이디 필드의 이름 */
   idName: Path<T>;
+  /** 도메인 필드의 이름 */
   domainName: Path<T>;
+  /** 아이디 입력 필드의 크기 */
   idInputSize?: string;
+  /** 도메인 입력 필드의 크기 */
   domainInputSize?: string;
+  /** 아이디 입력 필드의 스타일 */
   idInputStyle?: string;
+  /** 도메인 입력 필드의 스타일 */
   domainInputStyle?: string;
+  /** 아이디 텍스트 스타일 */
   idTextStyle?: string;
+  /** 도메인 텍스트 스타일 */
   domainTextStyle?: string;
+  /** 아이디 입력 테스트용 아이디 */
+  idTestId?: string;
+  /** 도메인 입력 테스트용 아이디 */
+  domainTestId?: string;
 };
 
 export default function EmailInput<T extends FieldValues>({
@@ -23,6 +36,8 @@ export default function EmailInput<T extends FieldValues>({
   domainInputStyle,
   idTextStyle,
   domainTextStyle,
+  idTestId,
+  domainTestId,
 }: EmailInputProps<T>) {
   return (
     <div className='flex items-center gap-[8px]'>
@@ -32,6 +47,7 @@ export default function EmailInput<T extends FieldValues>({
         size={idInputSize || 'w-[250px] h-[50px]'}
         inputStyle={idInputStyle}
         textStyle={idTextStyle}
+        testId={idTestId}
       />
       <span className='text-gray-90 text-headline1 font-normal'>@</span>
       <Input
@@ -40,6 +56,7 @@ export default function EmailInput<T extends FieldValues>({
         size={domainInputSize || 'w-[250px] h-[50px]'}
         inputStyle={domainInputStyle}
         textStyle={domainTextStyle}
+        testId={domainTestId}
       />
     </div>
   );
