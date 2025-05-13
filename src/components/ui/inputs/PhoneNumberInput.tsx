34630a855e46d569651f6653a6f5c7c5fc15ff4a
nninyeong <minyk0512@gmail.com>
2025-05-14 03:13:11 +0900

    chore: JSDocs 주석 위치 수정

--- src/components/ui/inputs/PhoneNumberInput.tsx
+++ src/components/ui/inputs/PhoneNumberInput.tsx
@@ -6,13 +6,21 @@ import { useRef, useState } from 'react';
 import Button from '../Button';
 
 type PhoneNumberInputProps<T extends FieldValues> = {
+  /** useForm에서 사용할 컨트롤러 */
   control: Control<T>;
+  /** 전화번호 앞자리 입력 필드의 이름 */
   prefixName: Path<T>;
+  /** 전화번호 뒷자리 입력 필드의 이름 */
   numberName: Path<T>;
+  /** 인증번호 확인 필드의 이름 */
   verificationName?: Path<T>;
+  /** 값 세팅을 위한 setValue 함수 */
   setValue?: (name: Path<T>, value: any) => void;
+  /** 전화번호 앞자리 입력 필드의 크기 */
   prefixInputSize?: string;
+  /** 전화번호 뒷자리 입력 필드의 크기 */
   numberInputSize?: string;
+  /** 전화번호 뒷자리 입력 필드의 테스트용 아이디 */
   numberTestId?: string;
 };
 
@@ -27,21 +35,13 @@ const getPrefixList = (field: ControllerRenderProps, instanceId: string) => {
 };
 
 export default function PhoneNumberInput<T extends FieldValues>({
-  /** useForm에서 사용할 컨트롤러 */
   control,
-  /** 전화번호 앞자리 입력 필드의 이름 */
   prefixName,
-  /** 전화번호 뒷자리 입력 필드의 이름 */
   numberName,
-  /** 전화번호 앞자리 입력 필드의 크기 */
   prefixInputSize,
-  /** 전화번호 뒷자리 입력 필드의 크기 */
   numberInputSize,
-  /** 인증번호 확인 필드의 이름 */
   verificationName,
-  /** 값 세팅을 위한 setValue 함수 */
   setValue,
-  /** 전화번호 뒷자리 입력 필드의 테스트용 아이디 */
   numberTestId,
 }: PhoneNumberInputProps<T>) {
   const instanceIdRef = useRef(crypto.randomUUID());
