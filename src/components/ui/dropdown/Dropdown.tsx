7a55699f0da6730b15f6ee4c7461659102efbe3e
nninyeong <minyk0512@gmail.com>
2025-05-13 12:18:01 +0900

    feat: 드롭다운 메뉴 (펼쳐지는 영역) 구현

--- src/components/ui/dropdown/Dropdown.tsx
+++ src/components/ui/dropdown/Dropdown.tsx
@@ -1,6 +1,12 @@
+'use client';
+
 import { ReactNode, useEffect, useRef, useState } from 'react';
 
+type DropdownType = 'default' | 'withInput';
+
 type DropdownProps = {
+  /** 드롭다운 타입, 기본값: default */
+  type?: DropdownType;
   /** 드롭다운 메뉴에 표시될 아이템 리스트 */
   menuList: {
     id: string | number;
@@ -9,24 +15,57 @@ type DropdownProps = {
   }[];
   /** 현재 선택된 메뉴를 관리하는 state */
   selectedItem: ReactNode;
-  /** 드롭다운 메뉴의 크기 (기본값: w-full h-full) */
+  /** 드롭다운 메뉴 한 칸의 크기 */
   size?: string;
   /** 닫힌 상태의 드롭다운 메뉴에 적용될 스타일 */
   closedAreaStyle?: string;
   /** 드롭다운 메뉴 아이템에 적용될 스타일 */
   menuItemStyle?: string;
+  /** 선텍된 아이템 텍스트에 적용될 스타일 */
+  selectedTextStyle?: string;
+  /** 드롭다운 메뉴 아이템 텍스트에 적용될 스타일 */
+  menuItemTextStyle?: string;
   /** 드롭다운이 열릴 때 실행될 콜백 함수 */
   onOpen?: () => void;
   /** 드롭다운이 닫힐 때 실행될 콜백 함수 */
   onClose?: () => void;
 };
 
+const DROPDOWN_SIZE = {
+  default: 'w-[119px] h-[40px]',
+  withInput: 'w-[120px] h-[55px]',
+} as const;
+
+const DROPDOWN_CLOSED_AREA_STYLE = {
+  default: {
+    base: 'border rounded-[5px] pl-[14px] pr-[42px] py-[12px]',
+    open: 'border-purple-50',
+    closed: 'border-gray-20',
+  },
+  withInput: {
+    base: 'border rounded-[10px] pl-[20px] pr-[56px]',
+    open: 'border-purple-50',
+    closed: 'border-gray-20',
+  },
+} as const;
+
+const DROPDOWN_TEXT_STYLE = {
+  default: 'text-gray-90 text-headline2 font-semibold',
+  withInput: 'text-gray-90 text-body-reading',
+} as const;
+
+const DROPDOWN_MENU_ITEM_STYLE = 'px-[10px] py-[5px]';
+const DROPDOWN_MENU_ITEM_TEXT_STYLE = 'text-headline2 font-normal';
+
 export default function Dropdown({
+  type = 'default',
   menuList,
   selectedItem,
-  size = 'w-full h-full',
-  closedAreaStyle = 'border border-gray-20 rounded-[5px] pl-[14px] pr-[42px]',
+  size,
+  closedAreaStyle,
   menuItemStyle,
+  selectedTextStyle,
+  menuItemTextStyle,
   onOpen,
   onClose,
 }: DropdownProps) {
@@ -67,7 +106,7 @@ export default function Dropdown({
 
   return (
     <div
-      className={`relative flex justify-start items-center ${size} ${closedAreaStyle} cursor-pointer select-none`}
+      className={`relative flex justify-start items-center ${size || DROPDOWN_SIZE[type]} ${closedAreaStyle || DROPDOWN_CLOSED_AREA_STYLE[type].base} ${isOpen ? DROPDOWN_CLOSED_AREA_STYLE[type].open : DROPDOWN_CLOSED_AREA_STYLE[type].closed} ${selectedTextStyle || DROPDOWN_TEXT_STYLE[type]}  cursor-pointer select-none`}
       ref={dropdownRef}
       onClick={handleToggle}
     >
@@ -77,6 +116,18 @@ export default function Dropdown({
         alt='dropdown 버튼'
         className='absolute right-[14px] w-[14px] h-[14px]'
       />
+      {isOpen && (
+        <div className='absolute top-full left-1/2 -translate-x-1/2 translate-y-[10.5px] flex flex-col justify-start items-start gap-[5px] px-[4px] py-[6px] bg-white rounded-[5px] shadow shadow-[0px_0px_7px_0px_rgba(70, 72, 84, 0.10)]'>
+          {menuList.map((menu) => (
+            <div
+              key={menu.id}
+              className={`${size} flex items-center ${menuItemStyle || DROPDOWN_MENU_ITEM_STYLE} ${menuItemTextStyle || DROPDOWN_MENU_ITEM_TEXT_STYLE}`}
+            >
+              {menu.label}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
