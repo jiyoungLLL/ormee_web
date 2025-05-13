e604302bf7e4c570f5b997cb79e4e9b65f02fea0
nninyeong <minyk0512@gmail.com>
2025-05-13 13:08:51 +0900

    test: disabled 상태 추가, storybook에서도 선택한 옵션이 유지되도록 setState를 핸들러로 설정

--- src/components/ui/dropdown/Dropdown.stories.tsx
+++ src/components/ui/dropdown/Dropdown.stories.tsx
@@ -17,14 +17,17 @@ type Story = StoryObj<typeof meta>;
 const DropdownTemplate = (args: any) => {
   const [selected, setSelected] = useState(args.selectedItem || '선택');
 
+  const menuListWithHandlers = args.menuList.map((item: any) => ({
+    ...item,
+    onClick: () => setSelected(item.label),
+  }));
+
   return (
-    <div className='flex justify-center items-center gap-[30px]'>
-      <div>선택된 메뉴: {selected}</div>
-      <Dropdown
-        {...args}
-        selectedItem={selected}
-      />
-    </div>
+    <Dropdown
+      {...args}
+      selectedItem={selected}
+      menuList={menuListWithHandlers}
+    />
   );
 };
 
@@ -48,15 +51,30 @@ export const WithInput: Story = {
   args: {
     type: 'withInput',
     menuList: [
-      { id: 'd1', label: '옵션 1' },
-      { id: 'd2', label: '옵션 2' },
-      { id: 'd3', label: '옵션 3' },
+      { id: 'd1', label: '010' },
+      { id: 'd2', label: '011' },
+      { id: 'd3', label: '02' },
     ],
-    selectedItem: '옵션 1',
+    selectedItem: '010',
   },
 };
 
-// 커스텀 스타일 드롭다운
+/** disabled 타입 드롭다운 */
+export const Disabled: Story = {
+  render: DropdownTemplate,
+  args: {
+    type: 'default',
+    disabled: true,
+    menuList: [
+      { id: 'd1', label: '메뉴 1' },
+      { id: 'd2', label: '메뉴 2' },
+      { id: 'd3', label: '메뉴 3' },
+    ],
+    selectedItem: '메뉴 1',
+  },
+};
+
+/** 커스텀 스타일 드롭다운 */
 export const CustomStyle: Story = {
   render: DropdownTemplate,
   args: {
