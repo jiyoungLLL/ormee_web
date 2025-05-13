225db1264f14d59641a82e9cc6174fa6a9429cdb
nninyeong <minyk0512@gmail.com>
2025-05-14 02:28:21 +0900

    feat: 폼제출, 에러처리 기능 추가

--- src/app/(header-layout)/signup/page.tsx
+++ src/app/(header-layout)/signup/page.tsx
@@ -2,10 +2,11 @@
 
 import Button from '@/components/ui/Button';
 import Input from '@/components/ui/Input';
-import PhoneNumberInput from '@/components/ui/PhoneNumberInput';
+import EmailInput from '@/components/ui/inputs/EmailInput';
+import PhoneNumberInput from '@/components/ui/inputs/PhoneNumberInput';
 import { PHONE_NUMBER_PREFIX, SignupFormValues, signupSchema } from '@/schemas/auth.schema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+import { FieldErrors, useForm } from 'react-hook-form';
 
 export default function SignUpPage() {
   const { control, handleSubmit, setValue } = useForm<SignupFormValues>({
@@ -31,8 +32,37 @@ export default function SignUpPage() {
     mode: 'onSubmit',
   });
 
+  const handleSingnUp = (data: SignupFormValues) => {
+    // TODO: 회원가입 요청 API 연동
+    alert('test');
+    alert(
+      `회원가입 요청\n아이디: ${data.id}\n비밀번호: ${data.password}\n비밀번호 확인: ${data.passwordConfirm}\n연락처 1: ${data.primaryPhone.prefix}-${data.primaryPhone.number}\n번호인증: ${data.isVerifiedPrimaryPhone}\n연락처 2: ${data.secondaryPhone?.prefix}-${data.secondaryPhone?.number}\n이메일: ${data.emailId}@${data.emailDomain}\n이름: ${data.name}\n영문명: ${data.englishName}`,
+    );
+  };
+
+  const handleSignupError = (errors: FieldErrors<SignupFormValues>) => {
+    const errorFields = Object.entries(errors);
+
+    if (errorFields.length > 0) {
+      const [field, error] = errorFields[0];
+
+      if (field === 'primaryPhone' || field === 'secondaryPhone') {
+        const phoneError = error as any;
+        if (phoneError.number) {
+          alert(phoneError.number.message);
+        } else if (phoneError.prefix) {
+          alert(phoneError.prefix.message);
+        }
+      } else if (error.message) {
+        alert(error.message);
+      }
+    }
+
+    // TODO: 예상치 못한 에러처리
+  };
+
   return (
-    <div className='w-[744.5px] h-full'>
+    <div className='w-[744.5px] h-full mb-[87px]'>
       <div className='flex justify-start items-end gap-[17px] mb-[30px]'>
         <h1 className='text-gray-90 text-[32px] font-bold leading-[44.8px] tracking-[-0.64px]'>회원가입</h1>
         <div className='flex justify-center items-center gap-[4px] py-[4px]'>
@@ -40,9 +70,9 @@ export default function SignUpPage() {
           <p className='text-body1-normal text-gray-60'>표시는 필수 사항입니다.</p>
         </div>
       </div>
-      <div className='flex flex-col justify-start items-start self-stretch w-full h-full px-[30px] py-[20px] rounded-[20px] bg-white'>
+      <div className='flex flex-col justify-start items-start self-stretch w-full h-full px-[30px] py-[20px] mb-[20px] rounded-[20px] bg-white'>
         <form className='w-full'>
-          <section className='w-full'>
+          <section className='w-full mb-[40px]'>
             <h2 className='mb-[30px] px-[5px] text-title3 font-bold text-gray-90'>회원정보</h2>
             <div className='grid grid-cols-[140px_1fr] gap-x-[12px] gap-y-[30px] items-start w-full'>
               <div className='flex items-start gap-[8px] py-[4px]'>
@@ -125,6 +155,20 @@ export default function SignUpPage() {
                 prefixName='secondaryPhone.prefix'
                 numberName='secondaryPhone.number'
               />
+              <div className='flex items-start gap-[8px] py-[4px]'>
+                <div className='top-0 left-[-12px] w-[6px] h-[6px] rounded-full bg-purple-50' />
+                <label
+                  className='text-gray-70 text-[18px] font-bold leading-[25.2px] tracking-[-0.36px]'
+                  htmlFor='passwordConfirm'
+                >
+                  이메일
+                </label>
+              </div>
+              <EmailInput
+                control={control}
+                idName='emailId'
+                domainName='emailDomain'
+              />
             </div>
           </section>
           <section className='w-full'>
@@ -162,6 +206,15 @@ export default function SignUpPage() {
           </section>
         </form>
       </div>
+      <Button
+        type={'BUTTON_BASE_TYPE'}
+        size='w-full h-[50px]'
+        title={'회원가입 완료하기'}
+        isPurple
+        isfilled
+        font='text-headline1 font-bold'
+        onClick={handleSubmit(handleSingnUp, handleSignupError)}
+      />
     </div>
   );
 }
