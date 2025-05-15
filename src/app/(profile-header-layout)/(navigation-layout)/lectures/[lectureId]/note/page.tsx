bc517e7b8ffba8d6429ee4798a96a048db463edc
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-05-15 16:09:07 +0900

    feat: 쪽지 ui 및 기능 구현

--- src/app/(profile-header-layout)/(navigation-layout)/lectures/[lectureId]/note/page.tsx
+++ src/app/(profile-header-layout)/(navigation-layout)/lectures/[lectureId]/note/page.tsx
@@ -1,3 +1,25 @@
+import NoteContent from '@/components/note/NoteContent';
+import Image from 'next/image';
+
 export default function Note() {
-  return <div>쪽지</div>;
+  return (
+    <>
+      <div className='flex items-end gap-[5px] h-[34px]'>
+        <div className='text-title3 font-bold flex items-center gap-[10px] px-[5px]'>
+          <Image
+            src={'/assets/icons/sidenav/note_selected.png'}
+            width={28}
+            height={28}
+            alt='쪽지 아이콘'
+            className='w-[28px] h-[28px]'
+          />
+          <div>쪽지</div>
+        </div>
+        <div className='text-label2-normal text-gray-60'>
+          어려웠거나 해설이 필요한 문장을 쪽지로 전달받을 수 있어요.
+        </div>
+      </div>
+      <NoteContent />
+    </>
+  );
 }
---
+++ src/components/note/ClosedNote.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ClosedNote from './ClosedNote';
+
+const meta = {
+  title: 'Components/Note/ClosedNote',
+  component: ClosedNote,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof ClosedNote>;
+
+export default meta;
+type Story = StoryObj<typeof ClosedNote>;
+
+export const Default: Story = {
+  args: {
+    noteKey: '10/29',
+    title: '10/29 쪽지',
+    date: '2024.10.29 15:00',
+  },
+};
