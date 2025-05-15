263c4ca3c9c9c696a2afddf82b16966d70b85312
nninyeong <minyk0512@gmail.com>
2025-05-15 22:58:37 +0900

    feat: 외부 클릭시 트리거는 무시하고 이벤트 발생하도록 설정

--- src/components/ui/dropdown/Dropdown.tsx
+++ src/components/ui/dropdown/Dropdown.tsx
@@ -1,10 +1,49 @@
 'use client';
 
-import { ReactNode, useEffect, useRef, useState } from 'react';
+import { ReactNode, RefObject, useEffect, useRef, useState } from 'react';
 
 type DropdownType = 'default' | 'withInput';
 
-type DropdownProps = {
+// type DropdownProps = {
+//   /** 드롭다운 타입, 기본값: default */
+//   type?: DropdownType;
+//   /** 드롭다운 메뉴에 표시될 아이템 리스트 */
+//   menuList: {
+//     id: string | number;
+//     label: ReactNode;
+//     onClick?: () => void;
+//   }[];
+//   /** 현재 선택된 메뉴를 관리하는 state */
+//   selectedItem: ReactNode;
+//   /** 드롭다운 메뉴 한 칸의 크기 */
+//   size?: string;
+//   /** 기본 트리거를 보여줄지 선택, 다른 요소 아래에 메뉴 리스트를 띄우고싶은 경우 false (기본값: true) */
+//   showTrigger?: boolean;
+//   /** showTrigger이 false일 경우 드롭다운 열림 상태를 관리할 state */
+//   isOpen?: boolean;
+//   /** 닫힌 상태의 드롭다운 메뉴에 적용될 스타일 */
+//   closedAreaStyle?: string;
+//   /** 드롭다운 메뉴 아이템에 적용될 스타일 */
+//   menuItemStyle?: string;
+//   /** 선텍된 아이템 텍스트에 적용될 스타일 */
+//   selectedTextStyle?: string;
+//   /** 드롭다운 메뉴 아이템 텍스트에 적용될 스타일 */
+//   menuItemTextStyle?: string;
+//   /** 드롭다운 메뉴 컨테이너의 최대 높이 */
+//   menuContainerMaxHeight?: number;
+//   /** 드롭다운이 열릴 때 실행될 콜백 함수 */
+//   onOpen?: () => void;
+//   /** 드롭다운이 닫힐 때 실행될 콜백 함수 */
+//   onClose?: () => void;
+//   /** 드롭다운 비활성화 여부 */
+//   disabled?: boolean;
+//   /** 드롭다운 트리거(항상 보이는 부분) 테스트 ID */
+//   triggerTestId?: string;
+//   /** 드롭다운 메뉴 컨테이너 테스트 ID (펼쳐진 영역) */
+//   menuContainerTestId?: string;
+// };
+
+type BaseDropdownProps = {
   /** 드롭다운 타입, 기본값: default */
   type?: DropdownType;
   /** 드롭다운 메뉴에 표시될 아이템 리스트 */
@@ -17,16 +56,10 @@ type DropdownProps = {
   selectedItem: ReactNode;
   /** 드롭다운 메뉴 한 칸의 크기 */
   size?: string;
-  /** 기본 트리거를 보여줄지 선택, 다른 요소 아래에 메뉴 리스트를 띄우고싶은 경우 false (기본값: true) */
-  showTrigger?: boolean;
-  /** showTrigger이 false일 경우 드롭다운 열림 상태를 관리할 state */
-  isOpen?: boolean;
-  /** 닫힌 상태의 드롭다운 메뉴에 적용될 스타일 */
-  closedAreaStyle?: string;
+  /** 드롭다운 메뉴 컨테이너의 position */
+  menuContainerPosition?: string;
   /** 드롭다운 메뉴 아이템에 적용될 스타일 */
   menuItemStyle?: string;
-  /** 선텍된 아이템 텍스트에 적용될 스타일 */
-  selectedTextStyle?: string;
   /** 드롭다운 메뉴 아이템 텍스트에 적용될 스타일 */
   menuItemTextStyle?: string;
   /** 드롭다운 메뉴 컨테이너의 최대 높이 */
@@ -37,11 +70,45 @@ type DropdownProps = {
   onClose?: () => void;
   /** 드롭다운 비활성화 여부 */
   disabled?: boolean;
-  /** 드롭다운 트리거(항상 보이는 부분) 테스트 ID */
-  triggerTestId?: string;
   /** 드롭다운 메뉴 컨테이너 테스트 ID (펼쳐진 영역) */
   menuContainerTestId?: string;
 };
+
+type WithTriggerProps = BaseDropdownProps & {
+  /** 기본 트리거를 보여줄지 선택 (true일 때) */
+  showTrigger: true;
+  /** 드롭다운 초기 열림 상태 */
+  initialOpen?: boolean;
+  /** 드롭다운 트리거에 적용될 스타일 */
+  triggerAreaStyle?: string;
+  /** 드롭다운 트리거(항상 보이는 부분) 테스트 ID */
+  triggerTestId?: string;
+  /** 선텍된 아이템 텍스트에 적용될 스타일 */
+  selectedTextStyle?: string;
+  /** showTrigger가 false일 경우에만 사용 가능 */
+  isOpen?: never;
+  /** 트리거로 사용할 요소의 ref */
+  triggerRef?: never;
+};
+
+type WithoutTriggerProps = BaseDropdownProps & {
+  /** 트리거로 사용할 요소의 ref */
+  triggerRef: RefObject<HTMLDivElement>;
+  /** 기본 트리거를 보여줄지 선택 (false일 때) */
+  showTrigger: false;
+  /** showTrigger이 false일 경우 드롭다운 열림 상태를 관리할 state */
+  isOpen: boolean;
+  /** 선텍된 아이템 텍스트에 적용될 스타일 */
+  selectedTextStyle?: never;
+  /** 드롭다운 트리거에 적용될 스타일 */
+  triggerAreaStyle?: never;
+  /** 드롭다운 트리거(항상 보이는 부분) 테스트 ID */
+  triggerTestId?: never;
+  /** showTrigger가 true일 경우에만 사용 가능 */
+  initialOpen?: never;
+};
+
+type DropdownProps = WithTriggerProps | WithoutTriggerProps;
 
 const DROPDOWN_SIZE = {
   default: 'w-[119px] h-[40px]',
@@ -73,33 +140,40 @@ const DROPDOWN_MENU_ITEM_TEXT_STYLE = 'text-headline2 font-normal';
 
 const DROPDOWN_MENU_MAX_HEIGHT = 200;
 
-export default function Dropdown({
-  type = 'default',
-  menuList,
-  selectedItem,
-  showTrigger = true,
-  isOpen: controlledIsOpen,
-  size,
-  closedAreaStyle,
-  menuItemStyle,
-  selectedTextStyle,
-  menuItemTextStyle,
-  menuContainerMaxHeight,
-  disabled,
-  onOpen,
-  onClose,
-  triggerTestId,
-  menuContainerTestId,
-}: DropdownProps) {
-  const [isOpenState, setIsOpenState] = useState(false);
-  const isOpen = showTrigger ? isOpenState : controlledIsOpen;
-  const triggerRef = useRef<HTMLDivElement>(null);
+export default function Dropdown(props: DropdownProps) {
+  const {
+    type = 'default',
+    menuList,
+    selectedItem,
+    size,
+    menuContainerPosition,
+    menuItemStyle,
+    selectedTextStyle,
+    menuItemTextStyle,
+    menuContainerMaxHeight,
+    disabled,
+    onOpen,
+    onClose,
+    menuContainerTestId,
+  } = props;
+
+  const showTrigger = 'showTrigger' in props ? props.showTrigger : true;
+
+  const controlledIsOpen = showTrigger === false ? props.isOpen : undefined;
+  const initialOpen = showTrigger === true ? props.initialOpen : undefined;
+  const triggerAreaStyle = showTrigger === true ? props.triggerAreaStyle : undefined;
+  const triggerTestId = showTrigger === true ? props.triggerTestId : undefined;
+  const triggerRef = showTrigger === false ? props.triggerRef : undefined;
+
+  const [isOpenState, setIsOpenState] = useState(initialOpen ?? false);
+
+  const defaultTriggerRef = useRef<HTMLDivElement>(null);
   const dropdownMenuRef = useRef<HTMLDivElement>(null);
 
   const handleToggle = () => {
     if (disabled) return;
 
-    const newOpenState = !isOpen;
+    const newOpenState = !isOpenState;
     setIsOpenState(newOpenState);
 
     if (newOpenState && onOpen) onOpen();
@@ -114,29 +188,34 @@ export default function Dropdown({
     if (onClose) onClose();
   };
 
+  const handleClose = () => {
+    showTrigger && setIsOpenState(false);
+    if (onClose) onClose();
+  };
+
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (triggerRef.current && triggerRef.current.contains(event.target as Node)) return;
+      if (defaultTriggerRef.current && defaultTriggerRef.current.contains(event.target as Node)) return;
+      if (triggerRef && triggerRef.current && triggerRef.current.contains(event.target as Node)) return;
 
       if (dropdownMenuRef.current && !dropdownMenuRef.current.contains(event.target as Node)) {
-        if (isOpen && onClose) onClose();
-        setIsOpenState(false);
+        handleClose();
       }
     };
 
-    isOpen && document.addEventListener('mousedown', handleClickOutside);
-    !isOpen && document.removeEventListener('mousedown', handleClickOutside);
+    isOpenState && document.addEventListener('mousedown', handleClickOutside);
+    !isOpenState && document.removeEventListener('mousedown', handleClickOutside);
 
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [isOpen, onClose]);
+  }, [isOpenState, onClose]);
 
   return (
-    <div className='relative'>
+    <div className='relative select-none'>
       {showTrigger && (
         <div
-          className={`flex justify-start items-center ${size || DROPDOWN_SIZE[type]} ${closedAreaStyle || DROPDOWN_CLOSED_AREA_STYLE[type].base} ${
+          className={`flex justify-start items-center ${size || DROPDOWN_SIZE[type]} ${triggerAreaStyle || DROPDOWN_CLOSED_AREA_STYLE[type].base} ${
             disabled
               ? DROPDOWN_CLOSED_AREA_STYLE[type].disabled
               : isOpenState
@@ -145,7 +224,7 @@ export default function Dropdown({
           } ${selectedTextStyle || DROPDOWN_TEXT_STYLE[type]} text-nowrap ${disabled ? 'cursor-not-allowed text-label-assistive' : 'cursor-pointer'} select-none`}
           onClick={handleToggle}
           data-testid={triggerTestId}
-          ref={triggerRef}
+          ref={defaultTriggerRef}
         >
           {selectedItem}
           <img
@@ -156,6 +235,24 @@ export default function Dropdown({
         </div>
       )}
       {isOpenState && (
+        <div
+          className={`absolute top-full left-1/2 -translate-x-1/2 translate-y-[10.5px] flex flex-col justify-start items-start gap-[5px] px-[4px] py-[6px] bg-white rounded-[5px] shadow shadow-[0px_0px_7px_0px_rgba(70, 72, 84, 0.10)] overflow-y-auto`}
+          style={{ maxHeight: menuContainerMaxHeight || DROPDOWN_MENU_MAX_HEIGHT }}
+          data-testid={menuContainerTestId}
+          ref={dropdownMenuRef}
+        >
+          {menuList.map((menu) => (
+            <div
+              key={menu.id}
+              className={`${size || DROPDOWN_SIZE[type]} flex items-center ${menuItemStyle || DROPDOWN_MENU_ITEM_STYLE} ${menuItemTextStyle || DROPDOWN_MENU_ITEM_TEXT_STYLE}`}
+              onClick={() => handleItemClick(menu.onClick)}
+            >
+              {menu.label}
+            </div>
+          ))}
+        </div>
+      )}
+      {controlledIsOpen && (
         <div
           className={`absolute top-full left-1/2 -translate-x-1/2 translate-y-[10.5px] flex flex-col justify-start items-start gap-[5px] px-[4px] py-[6px] bg-white rounded-[5px] shadow shadow-[0px_0px_7px_0px_rgba(70, 72, 84, 0.10)] overflow-y-auto`}
           style={{ maxHeight: menuContainerMaxHeight || DROPDOWN_MENU_MAX_HEIGHT }}
