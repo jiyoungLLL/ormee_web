9cabf87c2740d816d9a814c4107febc177f421b6
nninyeong <minyk0512@gmail.com>
2025-05-16 17:22:40 +0900

    fix: Problem type에서 id 제거 (useFeildArray id 사용)

--- src/types/quiz.types.ts
+++ src/types/quiz.types.ts
@@ -1,14 +1,11 @@
 export type ProblemType = 'choice' | 'essay';
 
 export type Problem = {
-  id: string;
   description: string;
   type: ProblemType;
   item: string[];
   answer: string;
 };
-
-export type ProblemRequest = Omit<Problem, 'id'> | { context: string };
 
 export type Quiz = {
   title: string;
@@ -22,5 +19,5 @@ export type QuizRequest = {
   description: string;
   created_time: string;
   due_time: string;
-  problems: ProblemRequest[];
+  problems: Problem[];
 };
