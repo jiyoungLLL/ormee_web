781b6a77e40c5b8ed1e91c491db8900964c9ef60
nninyeong <minyk0512@gmail.com>
2025-05-17 19:12:13 +0900

    feat: 정답 선택 라디오 버튼 추가

--- src/components/quiz/problem/ChoiceItemInput.tsx
+++ src/components/quiz/problem/ChoiceItemInput.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Input from '@/components/ui/Input';
+import Radio from '@/components/ui/radio/Radio';
 import { QuizFormValues } from '@/schemas/quiz.schema';
 import { useFieldArray, useFormContext } from 'react-hook-form';
 
@@ -10,19 +11,29 @@ type ChoiceItemInputProps = {
 };
 
 export default function ChoiceItemInput({ problemIndex, itemIndex }: ChoiceItemInputProps) {
-  const { control } = useFormContext<QuizFormValues>();
-  const { fields: itemFields, update } = useFieldArray({
+  const { control, register } = useFormContext<QuizFormValues>();
+  const { fields: problemFields } = useFieldArray({
     control,
-    name: `problems.${problemIndex}.item`,
+    name: `problems`,
   });
 
+  const item = problemFields[problemIndex].item[itemIndex].text;
+
   return (
-    <Input
-      control={control}
-      name={`problems.${problemIndex}.item.${itemIndex}.text`}
-      size='w-full h-[28px]'
-      placeholder='선지를 입력하세요.'
-      inputStyle='flex items-center bg-transparent focus:outline-none disabled:text-label-assistive'
-    />
+    <div className='flex items-center gap-[12px] w-full'>
+      <Radio
+        register={register}
+        name={`problems.${problemIndex}.answer`}
+        htmlFor={`radio-${problemIndex}-${itemIndex}`}
+        value={item}
+      />
+      <Input
+        control={control}
+        name={`problems.${problemIndex}.item.${itemIndex}.text`}
+        size='flex-1 w-full h-[28px]'
+        placeholder='선지를 입력하세요.'
+        inputStyle='flex items-center bg-transparent focus:outline-none disabled:text-label-assistive'
+      />
+    </div>
   );
 }
