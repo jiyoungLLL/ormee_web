fbda7d4b3307c022aa8bb7edcfc8cc8cc7fa513e
nninyeong <minyk0512@gmail.com>
2025-05-20 03:56:28 +0900

    feat: 퀴즈 리스트 목데이터와 msw용 모킹함수 추가

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -1,9 +1,17 @@
 import { http, HttpResponse } from 'msw';
 import { MOCK_NOTIFICATION_LIST_BULK } from './notification';
-
+import { QUIZ_LIST_RESPONSE_MIXED } from './quiz';
 export const handlers = [
   http.get('/api/teacher/notification/', () => {
     return HttpResponse.json(MOCK_NOTIFICATION_LIST_BULK, {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }),
+  http.get('/api/teachers/:lectureId/quizzes/', () => {
+    return HttpResponse.json(QUIZ_LIST_RESPONSE_MIXED, {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
