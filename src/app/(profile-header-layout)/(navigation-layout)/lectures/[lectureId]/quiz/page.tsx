429886d0cf0cee34f290792421329d9b1d8ca5dd
nninyeong <minyk0512@gmail.com>
2025-05-19 16:52:06 +0900

    feat: 퀴즈 페이지 레이아웃 설정

--- src/app/(profile-header-layout)/(navigation-layout)/lectures/[lectureId]/quiz/page.tsx
+++ src/app/(profile-header-layout)/(navigation-layout)/lectures/[lectureId]/quiz/page.tsx
@@ -1,3 +1,15 @@
 export default function QuizPage() {
-  return <div>퀴즈 페이지</div>;
+  return (
+    <div className='flex flex-col gap-[17px] max-w-[1018px] h-full'>
+      <div className='flex items-center px-[5px] gap-[10px]'>
+        <img
+          src='/assets/icons/sidenav/quiz_selected.png'
+          alt='퀴즈 페이지'
+          className='w-[28px] h-[28px]'
+        />
+        <span className='text-title3 font-bold'>퀴즈</span>
+      </div>
+      <div className='flex-1 flex flex-col gap-[20px] w-full h-full px-[30px] py-[20px] rounded-[20px] bg-white '></div>
+    </div>
+  );
 }
