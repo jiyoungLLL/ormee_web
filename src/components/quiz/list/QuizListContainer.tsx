d2978a3c1ee25e887802919fc216d9bdbb366c26
nninyeong <minyk0512@gmail.com>
2025-05-20 01:22:32 +0900

    feat: 퀴즈 생성 버튼 -> 퀴즈 생성폼으로 이동 구현

--- src/components/quiz/list/QuizListContainer.tsx
+++ src/components/quiz/list/QuizListContainer.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import Button from '@/components/ui/Button';
 import Dropdown from '@/components/ui/dropdown/Dropdown';
 import { MenuItem, useDropdown } from '@/hooks/ui/useDropdown';
+import Link from 'next/link';
 
 const QUIZ_DROPDOWN_LIST: MenuItem[] = [
   { id: 'quiz-list-total', label: '전체' },
@@ -16,11 +18,23 @@ export default function QuizListContainer() {
 
   return (
     <div className='flex-1 flex flex-col gap-[20px] w-full h-full px-[30px] py-[20px] rounded-[20px] bg-white'>
-      <Dropdown
-        showTrigger
-        menuList={quizCategoryList}
-        selectedItem={selectedQuizCategory}
-      />
+      <div className='flex justify-between items-center'>
+        <Dropdown
+          showTrigger
+          menuList={quizCategoryList}
+          selectedItem={selectedQuizCategory}
+        />
+        <Link href={`${window.location.pathname}/create`}>
+          <Button
+            type='BUTTON_CREATE_TYPE'
+            size='w-fit h-[49px]'
+            title='퀴즈 생성'
+            font='text-headline1 font-bold'
+            htmlType='button'
+            isPurple={false}
+          />
+        </Link>
+      </div>
       <div>{selectedQuizCategory}</div>
     </div>
   );
