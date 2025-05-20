3965e10afb6087c8719fa0a447848bfc5e360d06
nninyeong <minyk0512@gmail.com>
2025-05-21 00:58:27 +0900

    feat: 퀴즈 상태 put 요청 모킹 세팅

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -1,6 +1,8 @@
 import { http, HttpResponse } from 'msw';
 import { MOCK_NOTIFICATION_LIST_BULK } from './notification';
 import { QUIZ_LIST_RESPONSE_MIXED } from './quiz';
+import { QuizState } from '@/types/quiz.types';
+
 export const handlers = [
   http.get('/api/teacher/notification/', () => {
     return HttpResponse.json(MOCK_NOTIFICATION_LIST_BULK, {
@@ -17,5 +19,26 @@ export const handlers = [
         'Content-Type': 'application/json',
       },
     });
+  }),
+  http.put('/api/teachers/quizzes/:quizId/:state', ({ params }) => {
+    const { quizId, state } = params;
+    const quizIndex = QUIZ_LIST_RESPONSE_MIXED.findIndex((quiz) => quiz.id === quizId);
+
+    if (quizIndex === -1) {
+      return new HttpResponse(null, { status: 404 });
+    }
+
+    QUIZ_LIST_RESPONSE_MIXED[quizIndex] = {
+      ...QUIZ_LIST_RESPONSE_MIXED[quizIndex],
+      state: state as QuizState,
+      updated_at: new Date().toISOString(),
+    };
+
+    return HttpResponse.json(
+      { message: '퀴즈 상태 변경에 성공했습니다.' },
+      {
+        status: 200,
+      },
+    );
   }),
 ];
