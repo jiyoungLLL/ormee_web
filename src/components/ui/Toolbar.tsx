b6e76870a16c344a8383a272ec96b3f4f7640d48
nninyeong <minyk0512@gmail.com>
2025-05-21 04:07:04 +0900

    feat: 툴박스 목록 기능 토글 props 추가

--- src/components/ui/Toolbar.tsx
+++ src/components/ui/Toolbar.tsx
@@ -3,13 +3,14 @@ import Image from 'next/image';
 
 type ToolbarProps = {
   editor: Editor | null;
+  enableList?: boolean;
   enableImage?: boolean;
   containerStyle?: string;
 };
 
 const DEFAULT_CONTAINER_STYLE = 'w-[262px] h-[48px] py-[10px] flex gap-[31px] items-center';
 
-export default function Toolbar({ editor, enableImage = false, containerStyle }: ToolbarProps) {
+export default function Toolbar({ editor, enableImage = false, enableList = true, containerStyle }: ToolbarProps) {
   const textToolList = ['bold', 'italic', 'underlined'];
   const phraseToolList = ['list', 'list_numbered'];
   const imageToolList = ['image'];
@@ -30,10 +31,14 @@ export default function Toolbar({ editor, enableImage = false, containerStyle }:
         chain.toggleUnderline().run();
         break;
       case 'list':
-        chain.toggleBulletList().run();
+        if (enableList) {
+          chain.toggleBulletList().run();
+        }
         break;
       case 'list_numbered':
-        chain.toggleOrderedList().run();
+        if (enableList) {
+          chain.toggleOrderedList().run();
+        }
         break;
       case 'image':
         if (enableImage) {
@@ -73,8 +78,12 @@ export default function Toolbar({ editor, enableImage = false, containerStyle }:
   return (
     <div className={containerStyle || DEFAULT_CONTAINER_STYLE}>
       {renderToolBar(textToolList)}
-      <div className='bg-gray-30 h-[20px] w-[1px]'></div>
-      {renderToolBar(phraseToolList)}
+      {enableList && (
+        <>
+          <div className='bg-gray-30 h-[20px] w-[1px]'></div>
+          {renderToolBar(phraseToolList)}
+        </>
+      )}
       {enableImage && (
         <>
           <div className='bg-gray-30 h-[20px] w-[1px]'></div>
