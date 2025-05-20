10fa41c186a5fa575b5c7048255c05281a0bee28
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-05-20 14:32:02 +0900

    feat: 과제 생성 페이지

---
+++ src/app/(profile-header-layout)/(navigation-layout)/lectures/[lectureId]/homework/create/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Create from '@/components/ui/create/Create';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function HomeworkCreate() {
+  const pathname = usePathname();
+  // 뒤로가기 이벤트 감지?
+  useEffect(() => {}, [pathname]);
+
+  return (
+    <div>
+      <Create type='homework' />
+    </div>
+  );
+}
