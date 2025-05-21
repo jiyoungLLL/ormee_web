7c2fa32f64dc321cb98b36bfc4c6f1d50cc0c591
nninyeong <minyk0512@gmail.com>
2025-05-21 11:28:32 +0900

    feat: 퀴즈 응답 데이터 캬멜 <-> 스네이크 변환 함수 전체 학생, 제출 학생 추가

--- src/utils/transforms/quiz.transform.ts
+++ src/utils/transforms/quiz.transform.ts
@@ -6,5 +6,7 @@ export const transformQuizListToCamelCase = (response: QuizListResponse): QuizLi
     dueTime: quiz.due_time,
     limitTime: quiz.limit_time,
     updatedAt: quiz.updated_at,
+    submitStudents: quiz.submit_students,
+    totalStudents: quiz.total_students,
   }));
 };
