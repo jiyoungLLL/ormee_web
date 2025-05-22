5b056c0a76c60b0d35d5dc268450e0930e85befb
nninyeong <minyk0512@gmail.com>
2025-05-23 03:25:01 +0900

    feat: 진행 중인 퀴즈가 없을 때 임시 처리

--- src/components/quiz/list/OpenQuizList.tsx
+++ src/components/quiz/list/OpenQuizList.tsx
@@ -9,6 +9,11 @@ export default function OpenQuizList({ openQuizzes }: OpenQuizListProps) {
   return (
     <div className='flex flex-col gap-[20px] w-full '>
       <h2 className='text-heading2 font-semibold'>진행 퀴즈</h2>
+      {openQuizzes.length === 0 && (
+        <div className='flex justify-center items-center w-full h-[90px] text-heading2 font-semibold text-gray-50'>
+          진행 중인 퀴즈가 없어요.
+        </div>
+      )}
       {openQuizzes.map((quiz) => (
         <OpenQuizItem
           key={quiz.id}
