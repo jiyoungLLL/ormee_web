0bbb3124867b4b5812280479f3bb036c4b303cd9
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-05-23 15:53:06 +0900

    fix: props 누락

--- src/app/(profile-header-layout)/(navigation-layout)/lectures/[lectureId]/homework/create/page.tsx
+++ src/app/(profile-header-layout)/(navigation-layout)/lectures/[lectureId]/homework/create/page.tsx
@@ -1,17 +1,23 @@
 'use client';
 
 import Create from '@/components/ui/create/Create';
+import { useLectureId } from '@/hooks/queries/useLectureId';
 import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function HomeworkCreate() {
+  const lectureNum = useLectureId();
+
   const pathname = usePathname();
   // 뒤로가기 이벤트 감지?
   useEffect(() => {}, [pathname]);
 
   return (
     <div>
-      <Create type='homework' />
+      <Create
+        type='homework'
+        params={lectureNum}
+      />
     </div>
   );
 }
