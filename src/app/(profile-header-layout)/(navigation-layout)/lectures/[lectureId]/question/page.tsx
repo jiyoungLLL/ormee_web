d66c3e0fe62c600d50f180030dfdcf03f3dfb41e
nninyeong <minyk0512@gmail.com>
2025-05-24 02:19:34 +0900

    feat: 질문 목록 필터 버튼 구현

---
+++ src/app/(profile-header-layout)/(navigation-layout)/lectures/[lectureId]/question/page.tsx
@@ -0,0 +1,26 @@
+import QuestionContainer from '@/components/question/QuestionContainer';
+import QuestionFilterButton from '@/components/question/QuestionFilterButton';
+import QuestionFilterContextProvider from '@/components/question/QuestionFilterContextProvider';
+
+export default function QuestionPage() {
+  return (
+    <div className='flex flex-col max-w-[1018px] h-full'>
+      <div className='flex items-center px-[5px] gap-[10px] mb-[20px]'>
+        <img
+          src='/assets/icons/sidenav/question_selected.png'
+          alt='퀴즈 페이지'
+          className='w-[28px] h-[28px]'
+        />
+        <h1 className='text-title3 font-bold'>질문</h1>
+      </div>
+      <QuestionFilterContextProvider>
+        <div className='flex items-center gap-[10px] mb-[12px]'>
+          <QuestionFilterButton type='all' />
+          <QuestionFilterButton type='unanswered' />
+          <QuestionFilterButton type='answered' />
+        </div>
+        <QuestionContainer />
+      </QuestionFilterContextProvider>
+    </div>
+  );
+}
---
+++ src/components/question/QuestionFilterButton.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { QuestionListFilterType, useQuestionFilter } from '@/components/question/QuestionFilterContextProvider';
+
+type QuestionFilterButtonProps = {
+  type: QuestionListFilterType;
+};
+
+const BUTTON_LABEL_MAP: Record<QuestionFilterButtonProps['type'], string> = {
+  all: '전체',
+  unanswered: '답변 미등록',
+  answered: '답변 등록',
+};
+
+export default function QuestionFilterButton({ type }: QuestionFilterButtonProps) {
+  const { filter, setFilter } = useQuestionFilter();
+  const isSelected = filter === type;
+
+  const handleClick = () => {
+    setFilter(type);
+  };
+
+  return (
+    <button
+      type='button'
+      className={`${isSelected ? 'border-gray-70 bg-gray-70 font-semibold text-white' : 'border-gray-30 bg-transparent font-normal text-gray-60'} flex items-center px-[20px] py-[12px] border rounded-[25px] text-headline2`}
+      onClick={handleClick}
+    >
+      {BUTTON_LABEL_MAP[type]}
+    </button>
+  );
+}
