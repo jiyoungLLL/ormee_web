8b8aa2c5db49a0b29b64c042773f5b725961ad7f
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-05-28 17:50:52 +0900

    feat: 제출관리 탭

--- src/components/homework/HomeworkTap.tsx
+++ src/components/homework/HomeworkTap.tsx
@@ -1,65 +1,140 @@
 'use client';
 
+import { useLectureId } from '@/hooks/queries/useLectureId';
+import { MOCK_HOMEWORK } from '@/mock/homework';
+import { format } from 'date-fns';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
+import Button from '../ui/Button';
+import Dropdown from '../ui/dropdown/Dropdown';
+import HomeworkDetail from './HomeworkDetail';
 
 type HomeworkProps = {
   type: '전체' | '진행중' | '마감';
 };
 
-export default function HomeworkTap({ type }: HomeworkProps) {
-  const [isOpen, setIsOpen] = useState<{ [key in '진행중' | '마감']?: boolean }>({});
+const HOMEWORK = MOCK_HOMEWORK.data;
 
-  const toggleOpen = (name: '진행중' | '마감') => {
-    setIsOpen((prev) => ({
-      ...prev,
-      [name]: !prev[name],
-    }));
+export default function HomeworkTap({ type }: HomeworkProps) {
+  const lectureNum = useLectureId();
+  const [isOpen, setIsOpen] = useState<{ [key in '진행중' | '마감']?: number[] }>({});
+  const [selected, setSelected] = useState<Record<number, '전체' | '미제출' | '미확인'>>({});
+  const [selectedStudentIds, setSelectedStudentIds] = useState<string[]>([]);
+
+  const menuList = (id: number) => [
+    { id: 'hw-all', label: '전체', onClick: () => setSelected((prev) => ({ ...prev, [id]: '전체' })) },
+    { id: 'hw-submit', label: '미제출', onClick: () => setSelected((prev) => ({ ...prev, [id]: '미제출' })) },
+    { id: 'hw-check', label: '미확인', onClick: () => setSelected((prev) => ({ ...prev, [id]: '미확인' })) },
+  ];
+
+  const handleCheckedStudents = (ids: string[]) => {
+    setSelectedStudentIds(ids);
+  };
+
+  const handleHomeworkAlarm = () => {
+    alert(selectedStudentIds);
+  };
+
+  const toggleOpen = (name: '진행중' | '마감', id: number) => {
+    setIsOpen((prev) => {
+      const currentList = prev[name] || [];
+      const isAlreadyOpen = currentList.includes(id);
+      return {
+        ...prev,
+        [name]: isAlreadyOpen ? currentList.filter((openId) => openId !== id) : [...currentList, id],
+      };
+    });
   };
 
   const renderTap = (name: '진행중' | '마감') => {
     const badgeStyle = name === '진행중' ? 'bg-purple-15 text-purple-50' : 'bg-gray-10 text-gray-60';
-    const open = isOpen[name];
+    const openIds = isOpen[name] || [];
+    const validData = name === '진행중' ? HOMEWORK.openedAssignments : HOMEWORK.closedAssignments;
 
     return (
-      <div key={name}>
-        <div className='text-heading2 font-semibold'>{name === '마감' ? name : '진행'} 과제</div>
-        <div className='flex justify-between items-center px-[10px] py-[20px]'>
-          <div className='w-[509px] flex gap-[20px]'>
-            {name === '진행중' && (
-              <div className='w-[49px] h-[49px] bg-purple-10 rounded-[10px] flex justify-center items-center'>
-                <Image
-                  src='/assets/icons/category-icon/homework.png'
-                  width={24}
-                  height={24}
-                  alt='숙제 아이콘'
-                />
+      <div
+        key={name}
+        className='flex flex-col gap-[20px]'
+      >
+        <div className='text-heading2 font-semibold'>{name === '마감' ? name : '진행'} 숙제</div>
+        <div>
+          {validData.map((data, index) => (
+            <div key={`${data.id}-${data.title}`}>
+              <div className='flex justify-between items-center px-[10px] py-[20px]'>
+                <div className='w-[509px] flex gap-[20px]'>
+                  {name === '진행중' && (
+                    <div className='w-[49px] h-[49px] bg-purple-10 rounded-[10px] flex justify-center items-center'>
+                      <Image
+                        src='/assets/icons/category-icon/homework.png'
+                        width={24}
+                        height={24}
+                        alt='숙제 아이콘'
+                      />
+                    </div>
+                  )}
+                  <Link
+                    href={`/lectures/${lectureNum}/homework/detail?filter=${name === '진행중' ? 'ongoing' : 'done'}&id=${data.id}`}
+                    className='flex flex-col gap-[5px]'
+                  >
+                    <div className={`text-headline1 font-semibold ${name === '마감' && 'text-gray-60'}`}>
+                      {data.title}
+                    </div>
+                    <div className='text-label text-gray-50'>
+                      {data.openTime && data.dueTime
+                        ? `${format(data.openTime, 'yyyy.MM.dd')} - ${format(data.dueTime, 'yyyy.MM.dd')}`
+                        : ''}
+                    </div>
+                  </Link>
+                </div>
+                <div className='flex gap-[30px]'>
+                  <div className={`rounded-[24px] px-[15px] py-[6px] text-headline2 font-semibold ${badgeStyle}`}>
+                    {name}
+                  </div>
+                  <Image
+                    src='/assets/icons/sidenav/dropdown.png'
+                    width={24}
+                    height={24}
+                    alt='드롭다운 아이콘'
+                    className={`w-[24px] h-[24px] cursor-pointer transition-transform ${openIds.includes(data.id) ? 'rotate-180' : ''}`}
+                    onClick={() => toggleOpen(name, data.id)}
+                  />
+                </div>
               </div>
-            )}
-            <div className='flex flex-col gap-[5px]'>
-              <div className={`text-headline1 font-semibold ${name === '마감' && 'text-gray-60'}`}>과제 이름</div>
-              <div className='text-label text-gray-50'>날짜</div>
+              {openIds.includes(data.id) && (
+                <div className='px-[30px] py-[20px] mb-[20px] bg-gray-10 rounded-[15px] flex flex-col items-end'>
+                  <div className='w-[227px] flex justify-between'>
+                    <Dropdown
+                      showTrigger={true}
+                      menuList={menuList(data.id)}
+                      selectedItem={selected[data.id] || '전체'}
+                      triggerAreaOnOpenStyle='bg-gray-10'
+                      triggerAreaOnCloseStyle='bg-gray-10'
+                    />
+                    <Button
+                      type='BUTTON_BASE_TYPE'
+                      size='h-[40px]'
+                      font='text-headline2 font-semibold leading-[4px]'
+                      title='과제 알림'
+                      isPurple={true}
+                      onClick={handleHomeworkAlarm}
+                    />
+                  </div>
+                  <div className='w-full px-[10px] flex flex-col gap-[16px]'>
+                    <HomeworkDetail onCheckedStudentsChange={handleCheckedStudents} />
+                  </div>
+                </div>
+              )}
+              {index !== validData.length - 1 ? <div className='bg-gray-30 h-[1px] w-full'></div> : ''}
             </div>
-          </div>
-          <div className='flex gap-[30px]'>
-            <div className={`rounded-[24px] px-[15px] py-[6px] text-headline2 font-semibold ${badgeStyle}`}>{name}</div>
-            <Image
-              src='/assets/icons/sidenav/dropdown.png'
-              width={24}
-              height={24}
-              alt='드롭다운 아이콘'
-              className={`w-[24px] h-[24px] cursor-pointer transition-transform ${open ? 'rotate-180' : ''}`}
-              onClick={() => toggleOpen(name)}
-            />
-          </div>
+          ))}
         </div>
-        {open && <div className='px-[10px]'>열림요</div>}
       </div>
     );
   };
 
   return (
-    <div className='flex flex-col gap-[20px]'>
+    <div className='flex flex-col gap-[40px]'>
       {type === '전체' ? (
         <>
           {renderTap('진행중')}
--- src/components/homework/RenderHomework.tsx
+++ src/components/homework/RenderHomework.tsx
@@ -1,13 +1,47 @@
+'use client';
+
 import { useLectureId } from '@/hooks/queries/useLectureId';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import Button from '../ui/Button';
 import Dropdown from '../ui/dropdown/Dropdown';
 import HomeworkTap from './HomeworkTap';
 
 export default function RenderHomework() {
   const lectureNum = useLectureId();
-  const [selected, setSelected] = useState<'전체' | '진행중' | '마감'>('전체');
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const filterMap = {
+    전체: '',
+    진행중: 'ongoing',
+    마감: 'done',
+  };
+
+  const reverseFilterMap = {
+    ongoing: '진행중',
+    done: '마감',
+  } as const;
+
+  type FilterType = '전체' | '진행중' | '마감';
+
+  const paramFilter = searchParams.get('filter');
+  const initialFilter = (paramFilter && reverseFilterMap[paramFilter as keyof typeof reverseFilterMap]) || '전체';
+  const [selected, setSelected] = useState<FilterType>(initialFilter as FilterType);
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams.toString());
+    const mappedValue = filterMap[selected];
+
+    if (!mappedValue) {
+      params.delete('filter');
+    } else {
+      params.set('filter', mappedValue);
+    }
+
+    router.replace(`?${params.toString()}`);
+  }, [selected]);
 
   const menuList = [
     { id: 'hw-all', label: '전체', onClick: () => setSelected('전체') },
@@ -16,7 +50,7 @@ export default function RenderHomework() {
   ];
 
   return (
-    <div className='bg-white rounded-tr-[25px] rounded-b-[25px] px-[30px] py-[20px] flex flex-col gap-[20px]'>
+    <div className='bg-white max-h-[666px] rounded-tr-[25px] rounded-b-[25px] px-[30px] py-[20px] flex flex-col gap-[20px]'>
       {/* 과제 드롭다운 & 과제 생성 */}
       <div className='w-full flex justify-between'>
         <Dropdown
@@ -29,12 +63,12 @@ export default function RenderHomework() {
             type='BUTTON_CREATE_TYPE'
             size='h-[49px]'
             font='text-headline1 font-bold'
-            title='과제 생성'
+            title='숙제 생성'
             isPurple={true}
           />
         </Link>
       </div>
-      <div className='flex flex-col gap-[45px]'>
+      <div className='flex flex-col gap-[45px]  overflow-auto'>
         <HomeworkTap type={selected} />
       </div>
     </div>
