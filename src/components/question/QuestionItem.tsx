097d5e058777585b813a58bf78bfd0c21ab9e2dd
nninyeong <minyk0512@gmail.com>
2025-05-29 01:37:41 +0900

    feat: 질문 목록에서 상세페이지로 넘어가도록 Link 추가

--- src/components/question/QuestionItem.tsx
+++ src/components/question/QuestionItem.tsx
@@ -1,20 +1,28 @@
 'use client';
 
+import Link from 'next/link';
 import { Question } from '@/features/question/question.types';
 import { formatDatetimeWithoutTime } from '@/utils/date/formatDate';
+import { usePathname } from 'next/navigation';
 
 type QuestionItemProps = {
   question: Question;
 };
 
 export default function QuestionItem({ question }: QuestionItemProps) {
-  const { order, title, isAnswered, author, createdAt } = question;
+  const pathname = usePathname();
+  const { order, id, title, isAnswered, author, createdAt } = question;
   const formattedCreatedAt = formatDatetimeWithoutTime(createdAt);
 
   return (
     <article className='grid grid-cols-[34px_360px_55px_68px_82px] justify-between items-center w-full min-h-[50px]'>
       <span className='text-headline2 font-normal text-gray-70'>{order}</span>
-      <span className='text-headline2 font-semibold text-gray-90 cursor-pointer'>{title}</span>
+      <Link
+        href={`${pathname}/${id}`}
+        className='text-headline2 font-semibold text-gray-90 cursor-pointer'
+      >
+        {title}
+      </Link>
       <div
         className={`flex justify-center items-center w-[55px] px-[15px] py-[5px] rounded-[16px] text-body1-normal ${isAnswered ? 'bg-state-success text-[#5CB86C]' : 'bg-state-error text-[#CE5353]'}`}
       >
