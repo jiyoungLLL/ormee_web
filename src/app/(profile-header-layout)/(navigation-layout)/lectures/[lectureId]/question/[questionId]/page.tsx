5dbc12f9bf379f8ca51dbc9022bacafe211bd9da
nninyeong <minyk0512@gmail.com>
2025-05-29 01:51:44 +0900

    feat: 목록에서 상세페이지로 올 때 searchParams를 전달하고 그 조건으로 돌아올 수 있도록 설정

--- src/app/(profile-header-layout)/(navigation-layout)/lectures/[lectureId]/question/[questionId]/page.tsx
+++ src/app/(profile-header-layout)/(navigation-layout)/lectures/[lectureId]/question/[questionId]/page.tsx
@@ -1,16 +1,37 @@
 import QuestionDetail from '@/components/question/detail/QuestionDetail';
+import Link from 'next/link';
 
-export default function QuestionDetailPage() {
+export default function QuestionDetailPage({
+  params,
+  searchParams,
+}: {
+  params: { lectureId: string; questionId: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const { lectureId } = params;
+
+  const searchParamsString = new URLSearchParams();
+  Object.entries(searchParams).forEach(([key, value]) => {
+    if (value) {
+      searchParamsString.append(key, Array.isArray(value) ? value[0] : value);
+    }
+  });
+
+  const queryString = searchParamsString.toString() ? `?${searchParamsString.toString()}` : '';
+
   return (
     <div className='flex flex-col gap-[28px] w-full h-full'>
-      <div className='flex items-center gap-[15px] px-[5px]'>
+      <Link
+        href={`/lectures/${lectureId}/question${queryString}`}
+        className='flex items-center gap-[15px] px-[5px]'
+      >
         <img
           src='/assets/icons/left_arrow.png'
           className='w-[24px] h-[24px]'
           alt='질문 목록으로 돌아가기'
         />
         <h1 className='text-title3 font-bold'>질문</h1>
-      </div>
+      </Link>
       <div className='flex flex-col gap-[40px] w-full h-[710px] px-[30px] py-[20px] bg-white rounded-[20px]'>
         <QuestionDetail />
       </div>
--- src/components/question/QuestionItem.tsx
+++ src/components/question/QuestionItem.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { Question } from '@/features/question/question.types';
 import { formatDatetimeWithoutTime } from '@/utils/date/formatDate';
-import { usePathname } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 type QuestionItemProps = {
   question: Question;
@@ -11,6 +11,8 @@ type QuestionItemProps = {
 
 export default function QuestionItem({ question }: QuestionItemProps) {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
+
   const { order, id, title, isAnswered, author, createdAt } = question;
   const formattedCreatedAt = formatDatetimeWithoutTime(createdAt);
 
@@ -18,7 +20,7 @@ export default function QuestionItem({ question }: QuestionItemProps) {
     <article className='grid grid-cols-[34px_360px_55px_68px_82px] justify-between items-center w-full min-h-[50px]'>
       <span className='text-headline2 font-normal text-gray-70'>{order}</span>
       <Link
-        href={`${pathname}/${id}`}
+        href={`${pathname}/${id}?${searchParams}`}
         className='text-headline2 font-semibold text-gray-90 cursor-pointer'
       >
         {title}
