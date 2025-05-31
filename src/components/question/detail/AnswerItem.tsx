1183704f829a4182ee3201c293f0de298882f183
nninyeong <minyk0512@gmail.com>
2025-05-31 21:24:46 +0900

    feat: 댓글 추가 동작 버튼 -> 드롭다운 토글 구현

--- src/components/question/detail/AnswerItem.tsx
+++ src/components/question/detail/AnswerItem.tsx
@@ -1,25 +1,61 @@
 'use client';
 
+import Dropdown from '@/components/ui/dropdown/Dropdown';
 import { Answer } from '@/features/question/question.types';
+import { useDropdown } from '@/hooks/ui/useDropdown';
 import { formatDatetimeWithoutTime } from '@/utils/date/formatDate';
 import Image from 'next/image';
+import { useRef } from 'react';
 
 type AnswerItemProps = {
   answer: Answer;
 };
 
+const menuList = [
+  {
+    id: 'edit',
+    label: '답변 수정',
+  },
+  {
+    id: 'delete',
+    label: '답변 삭제',
+  },
+];
+
 export default function AnswerItem({ answer }: AnswerItemProps) {
   const { author, content, files, createdAt } = answer;
   const formattedCreatedAt = createdAt ? formatDatetimeWithoutTime(createdAt) : '';
 
+  const moreDropdownRef = useRef<HTMLDivElement>(null);
+  const { isOpen, handleToggle, menuListForDropdown } = useDropdown({ initialOpen: false, menuList });
+
   return (
     <div className='flex flex-col items-start gap-[15px] w-full px-[30px] py-[20px] rounded-[10px] bg-gray-10'>
-      <div className='flex justify-center items-center gap-[10px]'>
-        <div className='flex justify-center items-center gap-[5px] w-fit h-[28px] px-[10px] py-[3px] bg-purple-15 rounded-[5px]'>
-          <span className='text-headline1 font-semibold text-purple-50'>{author}</span>
-          <span className='text-headline1 font-normal text-purple-50'>선생님</span>
+      <div className='flex justify-between items-center w-full'>
+        <div className='flex justify-center items-center gap-[10px]'>
+          <div className='flex justify-center items-center gap-[5px] w-fit h-[28px] px-[10px] py-[3px] bg-purple-15 rounded-[5px]'>
+            <span className='text-headline1 font-semibold text-purple-50'>{author}</span>
+            <span className='text-headline1 font-normal text-purple-50'>선생님</span>
+          </div>
+          <span className='text-body2-normal text-gray-70'>{formattedCreatedAt}</span>
         </div>
-        <span className='text-body2-normal text-gray-70'>{formattedCreatedAt}</span>
+        <div ref={moreDropdownRef}>
+          <button
+            className='w-[24px] h-[24px] bg-center bg-no-repeat bg-contain'
+            style={{
+              backgroundImage: 'url(/assets/icons/More.png)',
+            }}
+            onClick={handleToggle}
+          />
+          <Dropdown
+            showTrigger={false}
+            triggerRef={moreDropdownRef}
+            isOpen={isOpen}
+            menuList={menuListForDropdown}
+            selectedItem={null}
+            menuContainerPosition='absolute top-full right-0'
+          />
+        </div>
       </div>
       <div className='text-body1-reading'>{content}</div>
       <div className='flex flex-wrap gap-[10px]'>
