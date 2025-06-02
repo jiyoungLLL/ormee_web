8b621ee69dbc1d40af5481af5af10bf456cce829
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-06-03 00:55:44 +0900

    refactor: queryKey 설정, onError 콜백 함수 추가

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,22 +1,43 @@
 import { fetcher, Method } from '@/utils/api/api';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-export function useApiQuery<T>(key: string[], endpoint: string, enabled = true) {
+export function useApiQuery<T>(
+  key: readonly unknown[],
+  endpoint: string,
+  enabled = true,
+  options?: {
+    staleTime?: number;
+    gcTime?: number;
+  },
+) {
   return useQuery<T>({
     queryKey: key,
     queryFn: () => fetcher<T>({ method: 'GET', endpoint }),
     enabled,
+    staleTime: options?.staleTime,
+    gcTime: options?.gcTime,
   });
 }
 
-export function useApiMutation<T, V>(method: Exclude<Method, 'GET'>, endpoint: string, onSuccess?: () => void) {
+export function useApiMutation<T, V>(
+  method: Exclude<Method, 'GET'>,
+  endpoint: string,
+  onSuccess?: () => void,
+  invalidateKey?: readonly unknown[] | readonly unknown[][],
+  onError?: (err: Error) => void,
+) {
   const queryClient = useQueryClient();
 
   return useMutation<T, Error, V>({
     mutationFn: (body: V) => fetcher<T>({ method, endpoint, body }),
     onSuccess: () => {
-      queryClient.invalidateQueries();
+      if (Array.isArray(invalidateKey)) {
+        invalidateKey.forEach((key) => queryClient.invalidateQueries({ queryKey: key }));
+      } else if (invalidateKey) {
+        queryClient.invalidateQueries({ queryKey: invalidateKey });
+      }
       onSuccess?.();
     },
+    onError,
   });
 }
