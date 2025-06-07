d8add0d0c1bea1f597cbe96505846f671bc64ad7
nninyeong <minyk0512@gmail.com>
2025-06-08 03:24:10 +0900

    feat: 퀴즈 생성 요청 성공시 퀴즈 목록 페이지로 이동

--- src/features/quiz/hooks/useQuiz.ts
+++ src/features/quiz/hooks/useQuiz.ts
@@ -1,17 +1,22 @@
+'use client';
+
 import { QUERY_KEYS } from '@/hooks/queries/queryKeys';
 import { useToastStore } from '@/stores/toastStore';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { QuizCreateRequest } from '@/features/quiz/quiz.types';
 import { postQuiz } from '@/features/quiz/api/quiz.api';
+import { useRouter } from 'next/navigation';
 
 export const usePostQuiz = ({ lectureId }: { lectureId: string }) => {
   const queryClient = useQueryClient();
   const { addToast } = useToastStore();
+  const router = useRouter();
 
   return useMutation({
     mutationFn: (quiz: QuizCreateRequest) => postQuiz(lectureId, quiz),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: QUERY_KEYS.quizList(lectureId) });
+      router.push(`/lectures/${lectureId}/quiz`);
       addToast({ message: '퀴즈가 생성되었어요.', type: 'success' });
     },
     onError: (error) => {
