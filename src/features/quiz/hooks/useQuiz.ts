db6aae866e3291f9c155fb0d85ead28b443f9ab4
nninyeong <minyk0512@gmail.com>
2025-06-09 00:20:45 +0900

    refactor: usePostQuiz onSuccess 함수 분리

--- src/features/quiz/hooks/useQuiz.ts
+++ src/features/quiz/hooks/useQuiz.ts
@@ -2,10 +2,28 @@
 
 import { QUERY_KEYS } from '@/hooks/queries/queryKeys';
 import { useToastStore } from '@/stores/toastStore';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { QueryClient, useMutation, useQueryClient } from '@tanstack/react-query';
 import { QuizCreateRequest } from '@/features/quiz/quiz.types';
 import { postQuiz } from '@/features/quiz/api/quiz.api';
 import { useRouter } from 'next/navigation';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { ToastData } from '@/types/toast.types';
+
+const handleQuizSuccess = (
+  quiz: QuizCreateRequest,
+  lectureId: string,
+  queryClient: QueryClient,
+  router: AppRouterInstance,
+  addToast: (toastData: Omit<ToastData, 'id'>) => void,
+) => {
+  const { isDraft } = quiz;
+  const queryKey = isDraft ? QUERY_KEYS.temporaryQuizList(lectureId) : QUERY_KEYS.quizList(lectureId);
+
+  queryClient.invalidateQueries({ queryKey });
+  router.push(`/lectures/${lectureId}/quiz`);
+
+  addToast({ message: `퀴즈가 ${isDraft ? '임시저장' : '생성'}되었어요.`, type: 'success' });
+};
 
 export const usePostQuiz = ({ lectureId }: { lectureId: string }) => {
   const queryClient = useQueryClient();
@@ -14,13 +32,7 @@ export const usePostQuiz = ({ lectureId }: { lectureId: string }) => {
 
   return useMutation({
     mutationFn: (quiz: QuizCreateRequest) => postQuiz(lectureId, quiz),
-    onSuccess: (_, quiz) => {
-      const { isDraft } = quiz;
-      const queryKey = isDraft ? QUERY_KEYS.temporaryQuizList(lectureId) : QUERY_KEYS.quizList(lectureId);
-      queryClient.invalidateQueries({ queryKey });
-      router.push(`/lectures/${lectureId}/quiz`);
-      addToast({ message: `퀴즈가 ${isDraft ? '임시저장' : '생성'}되었어요.`, type: 'success' });
-    },
+    onSuccess: (_, quiz) => handleQuizSuccess(quiz, lectureId, queryClient, router, addToast),
     onError: (error) => {
       addToast({ message: error.message, type: 'error' });
     },
