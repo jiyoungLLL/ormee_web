62f38216beeadc86f0f856a9ade67b1a65574d1f
nninyeong <minyk0512@gmail.com>
2025-06-08 23:39:55 +0900

    feat: 임시저장 여부에따라 쿼리키, 스키마, 토스트 메세지 달라지도록 구현

--- src/features/quiz/api/quiz.api.ts
+++ src/features/quiz/api/quiz.api.ts
@@ -1,12 +1,14 @@
 import { QuizCreateRequest } from '@/features/quiz/quiz.types';
-import { QuizCreateRequestSchema } from '@/features/quiz/quiz.schema';
+import { QuizCreateRequestSchema, QuizDraftRequestSchema } from '@/features/quiz/quiz.schema';
 
 export const postQuiz = async (lectureId: string, quiz: QuizCreateRequest) => {
-  const validatedQuiz = QuizCreateRequestSchema.safeParse(quiz);
+  const { isDraft } = quiz;
+  const schema = isDraft ? QuizDraftRequestSchema : QuizCreateRequestSchema;
+  const validatedQuiz = schema.safeParse(quiz);
 
   if (!validatedQuiz.success) {
     if (process.env.NODE_ENV === 'development') console.error(validatedQuiz.error);
-    throw new Error('퀴즈 생성에 실패했어요. 입력값을 확인해주세요.');
+    throw new Error(`퀴즈 ${isDraft ? '임시저장' : '생성'}에 실패했어요. 입력값을 확인해주세요. `);
   }
 
   const response = await fetch(`/api/teachers/${lectureId}/quizzes`, {
@@ -15,7 +17,7 @@ export const postQuiz = async (lectureId: string, quiz: QuizCreateRequest) => {
   });
 
   if (!response.ok) {
-    throw new Error('퀴즈 생성에 실패했어요. 다시 시도해주세요.');
+    throw new Error(`퀴즈 ${isDraft ? '임시저장' : '생성'}에 실패했어요. 다시 시도해주세요.`);
   }
 
   return response.json();
--- src/features/quiz/hooks/useQuiz.ts
+++ src/features/quiz/hooks/useQuiz.ts
@@ -14,10 +14,12 @@ export const usePostQuiz = ({ lectureId }: { lectureId: string }) => {
 
   return useMutation({
     mutationFn: (quiz: QuizCreateRequest) => postQuiz(lectureId, quiz),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.quizList(lectureId) });
+    onSuccess: (_, quiz) => {
+      const { isDraft } = quiz;
+      const queryKey = isDraft ? QUERY_KEYS.temporaryQuizList(lectureId) : QUERY_KEYS.quizList(lectureId);
+      queryClient.invalidateQueries({ queryKey });
       router.push(`/lectures/${lectureId}/quiz`);
-      addToast({ message: '퀴즈가 생성되었어요.', type: 'success' });
+      addToast({ message: `퀴즈가 ${isDraft ? '임시저장' : '생성'}되었어요.`, type: 'success' });
     },
     onError: (error) => {
       addToast({ message: error.message, type: 'error' });
