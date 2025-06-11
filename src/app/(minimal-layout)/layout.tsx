d388f5d84f852ab3b47786dae28db6f862e55b07
nninyeong <minyk0512@gmail.com>
2025-06-12 02:00:31 +0900

    fix: signin 페이지 Suspense로 래핑

--- src/app/(minimal-layout)/layout.tsx
+++ src/app/(minimal-layout)/layout.tsx
@@ -1,5 +1,19 @@
+'use client';
+
+import { Suspense } from 'react';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className='flex-1 flex flex-col justify-center items-center w-full h-full min-h-screen'>{children}</main>
+    <main className='flex-1 flex flex-col justify-center items-center w-full h-full min-h-screen'>
+      <Suspense
+        fallback={
+          <div className='flex items-center justify-center w-full h-full'>
+            <div className='w-8 h-8 border-4 border-gray-30 border-t-gray-30 rounded-full animate-spin'></div>
+          </div>
+        }
+      >
+        {children}
+      </Suspense>
+    </main>
   );
 }
