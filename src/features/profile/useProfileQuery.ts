3efe430be40a59fe33a6c921203b1e0fa11ef970
nninyeong <minyk0512@gmail.com>
2025-06-12 19:19:55 +0900

    feat: 서버액션 결과를 받아 에러처리 혹은 데이터를 반환하는 queryFn 추가

--- src/features/profile/useProfileQuery.ts
+++ src/features/profile/useProfileQuery.ts
@@ -5,10 +5,20 @@ import { useQuery } from '@tanstack/react-query';
 import { getProfileAction } from '@/features/profile/profile.action';
 import { UserProfileData } from './profile.types';
 
+export const getProfile = async () => {
+  const response = await getProfileAction();
+
+  if (response.status === 'error') {
+    throw new Error(response.message);
+  }
+
+  return response.data;
+};
+
 export const useGetProfileData = (options?: { initialData?: UserProfileData }) => {
-  return useQuery({
+  return useQuery<UserProfileData>({
     queryKey: QUERY_KEYS.profile(),
-    queryFn: getProfileAction,
+    queryFn: getProfile,
     staleTime: 1000 * 60 * 60, // 1시간
     gcTime: 1000 * 60 * 60 * 3, // 3시간
     ...options,
