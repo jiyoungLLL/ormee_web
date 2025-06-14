575c8819a35a8d427e852a3d3eb7019a6e098637
nninyeong <minyk0512@gmail.com>
2025-06-14 23:14:38 +0900

    feat: Api 성공 응답에 data가 없는 경우 제네릭을 지정하지 않으면 data 포함되지 않도록 수정

--- src/types/response.types.ts
+++ src/types/response.types.ts
@@ -1,3 +1,20 @@
+export type ApiResponse<T = void> = T extends void
+  ? {
+      status: 'success';
+      code: number;
+    }
+  :
+      | {
+          status: 'success';
+          code: number;
+          data: T;
+        }
+      | {
+          status: 'fail';
+          code: number;
+          data: string;
+        };
+
 export type ActionResponse<T> =
   | {
       status: 'success';
