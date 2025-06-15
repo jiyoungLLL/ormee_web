feaa34dd0f7f9f86378bf6d57ded684dd24f1e8e
nninyeong <minyk0512@gmail.com>
2025-06-16 00:49:16 +0900

    feat: 퀴즈 상세 데이터 prefetch

--- src/app/(profile-header-layout)/(navigation-layout)/lectures/[lectureId]/quiz/[quizId]/page.tsx
+++ src/app/(profile-header-layout)/(navigation-layout)/lectures/[lectureId]/quiz/[quizId]/page.tsx
@@ -1,3 +1,51 @@
-export default function QuizDetailPage() {
-  return <div></div>;
+'use server';
+
+import QuizDetailHeader from '@/components/quiz/\bdetail/QuizDetailHeader';
+import { QUERY_KEYS } from '@/hooks/queries/queryKeys';
+import { fetcher } from '@/utils/api/api';
+import { QueryClient } from '@tanstack/react-query';
+
+export default async function QuizDetailPage({ params }: { params: Promise<{ lectureId: string; quizId: string }> }) {
+  const { lectureId, quizId } = await params;
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 60 * 24,
+        gcTime: 1000 * 60 * 60 * 24,
+        retry: false,
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+      },
+    },
+  });
+
+  await queryClient.prefetchQuery({
+    queryKey: QUERY_KEYS.quizDetail(quizId),
+    queryFn: async () => {
+      const response = await fetcher({
+        method: 'GET',
+        endpoint: `/teachers/quizzes/${quizId}`,
+        authorization: true,
+      });
+
+      if (response.status === 'fail') {
+        Promise.reject(response.data);
+      }
+
+      if (response.status === 'success') {
+        return response.data;
+      }
+    },
+  });
+
+  return (
+    <div>
+      <QuizDetailHeader
+        lectureId={lectureId}
+        quizId={quizId}
+      />
+    </div>
+  );
 }
