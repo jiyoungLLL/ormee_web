f36152a6d2bd91226e952d65b785e730e07a3514
nninyeong <minyk0512@gmail.com>
2025-06-18 00:06:11 +0900

    feat: next/image 접근 패턴 설정

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,9 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      // TODO: 프로필 api 연동 후 테스트 이미지용 패턴 삭제
       {
         protocol: 'https',
-        hostname: 'img.freepik.com',
+        hostname: 'ormee-bucket.s3.ap-northeast-2.amazonaws.com',
       },
       // 피드백 임시 이미지
       { hostname: 'static.cdn.soomgo.com' },
