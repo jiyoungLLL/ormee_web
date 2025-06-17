0be7bd6a0d44c55db44949635595d71b2ee1448f
nninyeong <minyk0512@gmail.com>
2025-06-17 22:16:40 +0900

    feat: 에러 발생시 임시저장 목록에 에러메세지 표시

--- src/components/quiz/list/TemporaryQuizList.tsx
+++ src/components/quiz/list/TemporaryQuizList.tsx
@@ -6,10 +6,17 @@ import TemporaryQuizItem from '@/components/quiz/list/TemporaryQuizItem';
 
 export default function TemporaryQuizList() {
   const lectureId = useLectureId();
-  const { data } = useGetTemporaryQuizList(lectureId);
-  const temporaryQuizList = data?.openQuizzes.filter((quiz) => quiz.state === 'temporary') ?? [];
+  const { data: temporaryQuizList = [], error } = useGetTemporaryQuizList(lectureId);
 
-  const isLastQuiz = (index: number) => index === temporaryQuizList.length - 1;
+  const isLastQuiz = (index: number) => index === temporaryQuizList?.length - 1;
+
+  if (error) {
+    return (
+      <div className='flex justify-center items-center w-full h-full text-headline1 font-semibold text-gray-40'>
+        {error.message}
+      </div>
+    );
+  }
 
   return (
     <div className='flex flex-col gap-[5px] w-full'>
