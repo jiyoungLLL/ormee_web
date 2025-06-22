ff4a6f12b46e34f10552af40fcb8decb45bdb67e
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-06-22 18:57:43 +0900

    fix: storybook 오류

--- src/components/note/ClosedNote.stories.tsx
+++ src/components/note/ClosedNote.stories.tsx
@@ -13,10 +13,18 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof ClosedNote>;
 
+const handleClick = () => {
+  alert('드롭다운');
+};
+
 export const Default: Story = {
   args: {
     noteId: 1,
     title: '10/29 쪽지',
     date: '2024.10.29 15:00',
+    totalCount: 30,
+    submitCount: 12,
+    isOpen: false,
+    onClick: () => handleClick(),
   },
 };
