5c1c7925b538b15aeb8f4c3426badab2cdace2df
nninyeong <minyk0512@gmail.com>
2025-06-27 10:05:43 +0900

    chore: 프로필 목데이터 필드명 변경

--- src/mock/profile.ts
+++ src/mock/profile.ts
@@ -1,14 +1,14 @@
 import { UserProfileData } from '@/features/profile/profile.types';
 
 export const MOCK_USER_PROFILE: UserProfileData = {
-  name: '강수이',
+  nickname: '강수이',
   image:
     'https://img.freepik.com/premium-vector/girl-is-writing-chalkboard-with-pencil-her-hand_990404-19401.jpg?semt=ais_hybrid&w=740',
   bio: '가는 말이 고와야 오는 말이 곱다.',
 };
 
 export const MOCK_MINIMUM_USER_PROFILE: UserProfileData = {
-  name: '강수이',
+  nickname: '강수이',
   image: null,
   bio: null,
 };
