1fe671ac09f062274232d95cf0ce096cdc375ed7
nninyeong <minyk0512@gmail.com>
2025-06-30 00:58:04 +0900

    feat: 프로필 변경 요청 쿼리 구현

--- src/features/profile/useProfileQuery.ts
+++ src/features/profile/useProfileQuery.ts
@@ -1,9 +1,11 @@
 'use client';
 
 import { QUERY_KEYS } from '@/hooks/queries/queryKeys';
-import { ProfileResponse, UserProfileData } from '@/features/profile/profile.types';
-import { useApiQuery } from '@/hooks/useApi';
+import { ProfileEditRequest, ProfileResponse, UserProfileData } from '@/features/profile/profile.types';
+import { useApiMutation, useApiQuery } from '@/hooks/useApi';
 import { profileSchema } from '@/features/profile/profile.schema';
+import { ApiResponse } from '@/types/response.types';
+import { useToastStore } from '@/stores/toastStore';
 
 export const useGetProfileData = () => {
   return useApiQuery<ProfileResponse, UserProfileData>({
@@ -26,6 +28,27 @@ export const useGetProfileData = () => {
       refetchOnWindowFocus: false,
       refetchOnMount: true,
       refetchOnReconnect: false,
+    },
+  });
+};
+
+export const usePutProfileData = () => {
+  const { addToast } = useToastStore();
+
+  return useApiMutation<ApiResponse, ProfileEditRequest>({
+    method: 'PUT',
+    endpoint: '/teachers/profile',
+    fetchOptions: {
+      authorization: true,
+      errorMessage: '프로필 정보 수정이 실패했어요.',
+      contentType: 'application/json',
+    },
+    invalidateKey: QUERY_KEYS.profile(),
+    onSuccess: () => {
+      addToast({ message: '프로필 정보가 수정됐어요.', type: 'success' });
+    },
+    onError: (err) => {
+      addToast({ message: err.message, type: 'error' });
     },
   });
 };
