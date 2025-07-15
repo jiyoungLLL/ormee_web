3a48a21aa2dc9d204ea6098db480128ffc4c6b2d
nninyeong <minyk0512@gmail.com>
2025-07-16 01:35:31 +0900

    feat: 페이지 타이틀, 버튼, 콘텐츠 영역 배치

--- src/app/(profile-header-layout)/(navigation-layout)/mypage/personal/page.tsx
+++ src/app/(profile-header-layout)/(navigation-layout)/mypage/personal/page.tsx
@@ -1,3 +1,20 @@
-export default function Personal() {
-  return <div>마이페이지/개인정보 설정</div>;
+import Button from '@/components/ui/Button';
+
+export default function PersonalPage() {
+  return (
+    <div>
+      <div className='flex justify-between items-center mb-[25px]'>
+        <h1 className='text-title3 font-bold px-[5px]'>개인정보 설정</h1>
+        <Button
+          type='BUTTON_BASE_TYPE'
+          size='w-fit h-fit'
+          title='수정하기'
+          font='text-headline1 font-[18px] font-semibold'
+          isPurple
+          isfilled={false}
+        />
+      </div>
+      <div className='w-[1018px] h-[706px] px-[30px] py-[20px] rounded-[20px] bg-white'></div>
+    </div>
+  );
 }
