1f77218ce8c7d1d7ce6f4a9717bcc9e3c2acaf5a
nninyeong <minyk0512@gmail.com>
2025-07-18 11:10:34 +0900

    feat: 질문 상세 api 요청 연동

--- src/features/question/hooks/queries/useGetQuestionDetail.ts
+++ src/features/question/hooks/queries/useGetQuestionDetail.ts
@@ -1,17 +1,20 @@
-import { useQuery } from '@tanstack/react-query';
-import { Question } from '../../types/question.types';
 import { QUERY_KEYS } from '@/hooks/queries/queryKeys';
-import { getQuestionDetail } from '../../api/getQuestionDetail';
+import { useApiQuery } from '@/hooks/useApi';
+import { Question } from '@/features/question/types/question.types';
 
 export const useGetQuestionDetail = (questionId: string) => {
-  return useQuery<Question>({
+  return useApiQuery<Question>({
     queryKey: QUERY_KEYS.questionDetail(questionId),
-    queryFn: () => getQuestionDetail(questionId),
-    enabled: !!questionId,
-    staleTime: 1 * 60 * 60 * 1000,
-    gcTime: 5 * 60 * 60 * 1000,
-    refetchOnWindowFocus: false,
-    refetchOnMount: false,
-    refetchOnReconnect: false,
+    fetchOptions: {
+      endpoint: `/teachers/questions/${questionId}`,
+    },
+    queryOptions: {
+      enabled: !!questionId,
+      staleTime: 1 * 60 * 60 * 1000,
+      gcTime: 5 * 60 * 60 * 1000,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    },
   });
 };
