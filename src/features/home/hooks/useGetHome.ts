d4f4f8dd205c0776949b70a20535734925584df1
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-07-21 14:43:33 +0900

    refactor: 기존 사용했던 타입 -> home.schema.ts로 변경 및 삭제

--- src/features/home/hooks/useGetHome.ts
+++ src/features/home/hooks/useGetHome.ts
@@ -1,12 +1,14 @@
 'use client';
 
 import { HomeResponse } from '@/features/home/home.types';
-import { QUERY_KEYS } from '@/hooks/queries/queryKeys';
+import { useLectureId } from '@/hooks/queries/useLectureId';
 import { useApiQuery } from '@/hooks/useApi';
 
-export const useGetHome = (lectureId: string) => {
+export const useGetHome = () => {
+  const lectureId = useLectureId();
+
   return useApiQuery<HomeResponse>({
-    queryKey: QUERY_KEYS.home(lectureId),
+    queryKey: ['home', lectureId],
     fetchOptions: {
       endpoint: `/home/${lectureId}`,
       authorization: true,
