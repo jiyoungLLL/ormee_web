af41897db0fead6cb82f154e24164bbe62e90cde
jizero <163485562+jiyoungLLL@users.noreply.github.com>
2025-07-26 20:10:39 +0900

    feat: 공동작업자 컴포넌트 분리

---
+++ src/components/class/Coworker.tsx
@@ -0,0 +1,87 @@
+import { useDeleteCoworker, usePostCoworker } from '@/features/class/hooks/queries/useClassApi';
+import { useModal } from '@/hooks/ui/useModal';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import Button from '../ui/Button';
+import Input from '../ui/Input';
+import Modal from '../ui/Modal';
+
+type CoworkerProps = {
+  lectureId: string;
+  post: boolean;
+};
+
+export default function Coworker({ lectureId, post }: CoworkerProps) {
+  const [isPost, setIsPost] = useState<boolean>(post);
+
+  const [confirmModal, setConfirmModal] = useState<boolean>(false);
+  const { isOpen, closeModal } = useModal({ defaultOpen: false });
+
+  const methods = useForm({
+    mode: 'onSubmit',
+  });
+
+  const { control, watch, handleSubmit, reset } = methods;
+
+  const username = watch('coworker');
+
+  const postCoworker = usePostCoworker(lectureId, username, () => setIsPost(false));
+  const deleteCoworker = useDeleteCoworker(lectureId, username, () => setIsPost(true));
+
+  const onSubmit = () => {
+    if (isPost) {
+      postCoworker.mutate(lectureId, username);
+    } else {
+      deleteCoworker.mutate(lectureId, username);
+      reset({ coworker: '' });
+    }
+    closeModal();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className='flex flex-col gap-[10px] text-headline2 font-semibold'
+    >
+      공동작업자 관리
+      <div className='flex gap-[10px]'>
+        <Input
+          name='coworker'
+          control={control}
+          size='w-full h-[50px]'
+          placeholder='아이디 입력'
+          type='text'
+        />
+
+        <Button
+          type='BUTTON_BASE_TYPE'
+          size='h-[50px]'
+          font='text-healine1 font-bold'
+          title={isPost ? '추가' : '삭제'}
+          isPurple={true}
+          isfilled={false}
+          htmlType='button'
+          onClick={() => {
+            if (isPost) {
+              onSubmit();
+            } else {
+              setConfirmModal(true);
+            }
+          }}
+        />
+
+        {!isPost && confirmModal && (
+          <Modal
+            isOpen={isOpen}
+            onCancel={closeModal}
+            onConfirm={onSubmit}
+            title='공동작업자를 삭제할까요?'
+            iconSrc='/assets/icons/warning.png'
+            description='공동작업자 변경은 강의실 당 1회에 한해 가능해요'
+            confirmButtonType={{ isPurple: true }}
+          />
+        )}
+      </div>
+    </form>
+  );
+}
